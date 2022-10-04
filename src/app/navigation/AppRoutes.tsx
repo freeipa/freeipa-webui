dd43266d59f470e612842553f759df3a223ad31a
Carla Martinez <carlmart@redhat.com>
2022-10-04 16:41:55 +0200

    Implement Context and custom route functionality
    A way to tell what navigation elements (in terms of Patternfly
    components) are active and manage some data related to this fact is
    needed.
    
    In order to do so, some global variables (and its setter functions)
    have been defined:
      - `groupActive` will tell the current active group. That means that
        this value will change every time a new navigation element is clicked.
        Its setter function is `setGroupActive`.
      - `superGroupActive` is similar to the `groupActive` one, but it takes
        into account the upper-level component that is active. This is
        useful to highlight the parent section when the navigation menu is
        collapsed. Eg: In `Identity` > `Users` > `Active users`, if we
        decide not to set the `superGroupActive` variable, `Active users` will
        be highlighted but its parent (`Users`) won't. Its setter function
        is `setSuperGroupActive`.
      - `browserTitle` refers to the title shown in the browser tab. Its
        setter function is `setBrowserTitle`.
    
    The aforementioned variables are taking the values defined in the
    `NavRoutes.ts` structure file.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/app/navigation/AppRoutes.tsx
+++ src/app/navigation/AppRoutes.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
+// React router dom
 import { Navigate, Route, Routes } from "react-router-dom";
 import NotFound from "./NotFound";
-// import { useDocumentTitle } from "./utils/useDocumentTitle";
 
 // PAGE COMPONENTS
 import ActiveUsers from "app/pages/ActiveUsers/ActiveUsers";
 import ActiveUsersTabs from "app/pages/ActiveUsers/ActiveUsersTabs";
 import StageUsers from "app/pages/StageUsers/StageUsers";
+import StageUsersTabs from "app/pages/StageUsers/StageUsersTabs";
 import PreservedUsers from "app/pages/PreservedUsers/PreservedUsers";
-// import ActiveUsersSettings from "./pages/ActiveUsersSettings";
-// import ActiveUsersIsMemberOf from "./pages/ActiveUsersIsMemberOf";
-// import ActiveUsersIsMemberOfAdd from "./pages/ActiveUsersIsMemberOfAdd";
+import UserGroups from "app/pages/UserGroups/UserGroups";
+import HostGroups from "app/pages/HostGroups/HostGroups";
+import Netgroups from "app/pages/Netgroups/Netgroups";
+
 // Navigation
 import { URL_PREFIX } from "./NavRoutes";
 
-// Renders routes
+// Renders routes (React)
 export const AppRoutes = (): React.ReactElement => (
   <Routes>
     <Route path={URL_PREFIX}>
@@ -24,9 +26,64 @@ export const AppRoutes = (): React.ReactElement => (
       </Route>
       <Route path="stage-users">
         <Route path="" element={<StageUsers />} />
+        <Route path="settings" element={<StageUsersTabs />} />
       </Route>
       <Route path="preserved-users">
         <Route path="" element={<PreservedUsers />} />
+      </Route>
+      <Route path="hosts">
+        <Route path="" />
+      </Route>
+      <Route path="services">
+        <Route path="" />
+      </Route>
+      <Route path="user-groups">
+        <Route path="" element={<UserGroups />} />
+      </Route>
+      <Route path="host-groups">
+        <Route path="" element={<HostGroups />} />
+      </Route>
+      <Route path="netgroups">
+        <Route path="" element={<Netgroups />} />
+      </Route>
+      <Route path="id-views">
+        <Route path="" />
+      </Route>
+      <Route path="user-group-rules">
+        <Route path="" />
+      </Route>
+      <Route path="host-group-rules">
+        <Route path="" />
+      </Route>
+      <Route path="hbac-rules">
+        <Route path="" />
+      </Route>
+      <Route path="hbac-services">
+        <Route path="" />
+      </Route>
+      <Route path="hbac-service-groups">
+        <Route path="" />
+      </Route>
+      <Route path="hbac-test">
+        <Route path="" />
+      </Route>
+      <Route path="sudo-rules">
+        <Route path="" />
+      </Route>
+      <Route path="sudo-commands">
+        <Route path="" />
+      </Route>
+      <Route path="sudo-command-group">
+        <Route path="" />
+      </Route>
+      <Route path="selinux-user-maps">
+        <Route path="" />
+      </Route>
+      <Route path="password-policies">
+        <Route path="" />
+      </Route>
+      <Route path="kerberos-ticket-policy">
+        <Route path="" />
       </Route>
     </Route>
     <Route
--- src/app/navigation/Nav.tsx
+++ src/app/navigation/Nav.tsx
@@ -1,47 +1,71 @@
 import { Nav, NavExpandable, NavItem, NavList } from "@patternfly/react-core";
-import React, { useState } from "react";
-import {
-  NavLink,
-  useInRouterContext,
-  useLocation,
-  useMatch,
-  useResolvedPath,
-} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 // Navigation (PatternFly)
 import { navigationRoutes } from "./NavRoutes";
+// Context
+import { Context } from "app/App";
 
-const renderNavItem = (item: { label: string; url: string }, id: number) => {
-  const location = useLocation();
-  console.log(useMatch(item.url));
-
+// Renders NavItem
+const renderNavItem = (
+  item: { label: string; group: string; path: string; title: string },
+  id: number,
+  superGroup: string
+) => {
+  const { groupActive, setGroupActive, setBrowserTitle, setSuperGroupActive } =
+    React.useContext(Context);
   return (
-    <NavItem key={id} isActive={!!useMatch(item.url)}>
-      <NavLink to={item.url}>{item.label}</NavLink>
+    <NavItem
+      key={id}
+      isActive={groupActive === item.group}
+      onClick={() => {
+        setGroupActive(item.group);
+        setBrowserTitle(item.title);
+        setSuperGroupActive(superGroup);
+      }}
+    >
+      <NavLink to={item.path}>{item.label}</NavLink>
     </NavItem>
   );
 };
 
+// Renders 'Navigation'
 const Navigation = () => {
+  const { superGroupActive } = React.useContext(Context);
   return (
     <Nav>
       <NavList>
         {navigationRoutes.map((section, id) => {
           return (
-            <NavExpandable key={id} title={section.label} isActive isExpanded>
+            <NavExpandable
+              key={id}
+              title={section.label}
+              isActive={section.items.some(
+                (route) => route.group === superGroupActive
+              )}
+              isExpanded={section.items.some(
+                (route) => route.group === superGroupActive
+              )}
+            >
               {section.items.map((subsection, sid) => {
-                return (
-                  <NavExpandable
-                    key={sid}
-                    title={subsection.label}
-                    isActive
-                    isExpanded
-                  >
-                    {subsection.items.map(
-                      (linkItem, lid) =>
-                        linkItem.url && renderNavItem(linkItem, lid)
-                    )}
-                  </NavExpandable>
-                );
+                if (subsection.items.length > 0) {
+                  return (
+                    <NavExpandable
+                      key={sid}
+                      title={subsection.label}
+                      isActive={superGroupActive === subsection.group}
+                      isExpanded={superGroupActive === subsection.group}
+                    >
+                      {subsection.items.map(
+                        (linkItem, lid) =>
+                          linkItem.path &&
+                          renderNavItem(linkItem, lid, subsection.group)
+                      )}
+                    </NavExpandable>
+                  );
+                } else {
+                  return renderNavItem(subsection, sid, subsection.group);
+                }
               })}
             </NavExpandable>
           );
