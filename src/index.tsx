cb390f9284d7463a78fc296010d5aacf6663ca37
Carla Martinez <carlmart@redhat.com>
2022-11-02 11:23:53 +0100

    Add redux and utils
    Redux infrastructure is necessary to have a centralized state data store
    and be able to reuse several data states in the project. To manage the
    users, the `users` slice related data and its setters functions
    (add, delete, and change the status) is needed.
    
    Also, to prevent passing multiple variables and its setters via props to
    any component, the `shared` slice has been created. This way, the amount
    of data passed though components is considerably decreased, making the
    structure leaner and more readable (This will be reflected on the
    upcoming commits).
    
    E.g:
       Before:
                <ActiveUsersTable
                    activeUsersList={activeUsersList}
                    shownUsersList={shownUsersList}
                    isUserSelectable={isUserSelectable}
                    selectedUserNames={selectedUserNames}
                    changeSelectedUserNames={changeSelectedUserNames}
                    selectedUserIds={selectedUserIds}
                    changeSelectedUserIds={changeSelectedUserIds}
                    selectableUsers={selectableUsersTable}
                    checkEqualStatus={checkEqualStatus}
                    setUserSelected={setUserSelected}
                    changeSelectedUsers={updateSeletedUsers}
                    selectedPerPage={selectedPerPage}
                    changeSelectedPerPage={changeSelectedPerPage}
                    changeIsDeleteButtonDisabled={updateIsDeleteButtonDisabled}
                    changeIsEnableButtonDisabled={updateIsEnableButtonDisabled}
                    changeIsDisableButtonDisabled={updateIsDisableButtonDisabled}
                    isDeletion={isDeletion}
                    updateIsDeletion={updateIsDeletion}
                    isDisableEnableOp={isDisableEnableOp}
                    updateIsDisableEnableOp={updateIsDisableEnableOp}
                    showTableRows={showTableRows}
                />
       After:
                <ActiveUsersTable
                    elementsList={activeUsersList}
                    shownElementsList={shownUsersList}
                    isUserSelectable={isUserSelectable}
                    selectedUserNames={selectedUserNames}
                    changeSelectedUserNames={changeSelectedUserNames}
                    selectableUsers={selectableUsersTable}
                    setUserSelected={setUserSelected}
                />
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,23 @@ import App from "./App";
 import "./index.css";
 // react router dom
 import { BrowserRouter } from "react-router-dom";
+// Redux
+import store from "./store/store";
+import { Provider } from "react-redux";
+// PatternFly utilities
+import "@patternfly/patternfly/utilities/Spacing/spacing.css";
+import "@patternfly/patternfly/utilities/Text/text.css";
+import "@patternfly/patternfly/utilities/Sizing/sizing.css";
+import "@patternfly/patternfly/utilities/Display/display.css";
+import "@patternfly/patternfly/utilities/Accessibility/accessibility.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/utils/datatypes/globalDataTypes.ts
@@ -0,0 +1,11 @@
+export interface User {
+  userId: string;
+  userLogin: string;
+  firstName: string;
+  lastName: string;
+  status: string;
+  uid: string;
+  emailAddress: string;
+  phone: string;
+  jobTitle: string;
+}
