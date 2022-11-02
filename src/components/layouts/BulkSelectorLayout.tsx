f94982b11513e6c47dff68aeb5b5983dadaee0ce
Carla Martinez <carlmart@redhat.com>
2022-11-02 11:36:16 +0100

    Create Layouts for the components
    To make the components leaner and reusable, some Layouts need to be
    created. This will also help to have a set of atomic elements that can
    be used anywhere in the application.
    
    The list of Layouts created is the following:
    - `BulkSelectorLayout`: For the Bulk selector component (on Toolbar)
    - `HelpTextWithIconLayout`: Question mark with the 'Help' title (Toolbar)
    - `KebabLayout`: Kebab component
    - `ModalWithFormLayout`: Modal with form
    - `PaginationLayout`: Pagination component
    - `SecondaryButton`: Secondary button (blue)
    - `SkeletonLayout`: Skeleton as a placeholder in table rows
    - `SkeletonOnTableLayout`: Will show N skeletons rows (being N defined
       on the same component)
    - `TableLayout`: Table component
    - `TitleLayout`: Title component
    - `ToolbarLayout`: Toolbar component
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/components/layouts/BulkSelectorLayout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+// PatternFly
+import { Popper } from "@patternfly/react-core";
+
+interface PropsToBulkSelector {
+  menuKey: string;
+  selectorClassName?: string;
+  containerRefMenu?: React.RefObject<HTMLDivElement>;
+  toggle?: JSX.Element;
+  menuToolbar: JSX.Element;
+  appendTo?: HTMLElement | (() => HTMLElement) | undefined;
+  isOpenMenu?: boolean;
+  ariaLabel?: string;
+}
+
+const BulkSelectorLayout = (props: PropsToBulkSelector) => {
+  return (
+    <div
+      key={props.menuKey}
+      ref={props.containerRefMenu}
+      className={props.selectorClassName}
+    >
+      <Popper
+        trigger={props.toggle}
+        popper={props.menuToolbar}
+        appendTo={props.appendTo}
+        isVisible={props.isOpenMenu}
+        aria-label={props.ariaLabel}
+      />
+    </div>
+  );
+};
+
+export default BulkSelectorLayout;
