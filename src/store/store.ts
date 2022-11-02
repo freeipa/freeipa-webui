freeipa/freeipa-webui

commit 1ba0ad0c2634e72cbb3c5c464767146f1a2dda7f

Add redux and utils
Redux infrastructure is necessary to have a centralized state data store
and be able to reuse several data states in the project. To manage the
users, the `users` slice related data and its setters functions
(add, delete, and change the status) is needed.

Also, to prevent passing multiple variables and its setters via props to
any component, the `shared` slice has been created. This way, the amount
of data passed though components is considerably decreased, making the
structure leaner and more readable (This will be reflected on the
upcoming commits).

E.g:
   Before:
            <ActiveUsersTable
                activeUsersList={activeUsersList}
                shownUsersList={shownUsersList}
                isUserSelectable={isUserSelectable}
                selectedUserNames={selectedUserNames}
                changeSelectedUserNames={changeSelectedUserNames}
                selectedUserIds={selectedUserIds}
                changeSelectedUserIds={changeSelectedUserIds}
                selectableUsers={selectableUsersTable}
                checkEqualStatus={checkEqualStatus}
                setUserSelected={setUserSelected}
                changeSelectedUsers={updateSeletedUsers}
                selectedPerPage={selectedPerPage}
                changeSelectedPerPage={changeSelectedPerPage}
                changeIsDeleteButtonDisabled={updateIsDeleteButtonDisabled}
                changeIsEnableButtonDisabled={updateIsEnableButtonDisabled}
                changeIsDisableButtonDisabled={updateIsDisableButtonDisabled}
                isDeletion={isDeletion}
                updateIsDeletion={updateIsDeletion}
                isDisableEnableOp={isDisableEnableOp}
                updateIsDisableEnableOp={updateIsDisableEnableOp}
                showTableRows={showTableRows}
            />
   After:
            <ActiveUsersTable
                elementsList={activeUsersList}
                shownElementsList={shownUsersList}

src/store/store.ts
@@ -1,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./Identity/users-slice";
+import sharedReducer from "./shared/shared-slice";
+// import userGroupsReducer from "./Identity/userGroups-slice";
+// import netgroupsReducer from "./Identity/netgroups-slice";
+// import rolesReducer from "./IPA server/roles-slice";
+// import hbacRulesReducer from "./Policy/hbacRules-slice";
+// import sudoRulesReducer from "./Policy/sudoRules-slice";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    shared: sharedReducer,
+    // usergroups: userGroupsReducer,
+    // netgroups: netgroupsReducer,
+    // roles: rolesReducer,
+    // hbacrules: hbacRulesReducer,
+    // sudorules: sudoRulesReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;

src/index.tsx
@@ -4,12 +4,23 @@ import App from "./App";
 import "./index.css";
 // react router dom
 import { BrowserRouter } from "react-router-dom";
+// Redux
+import store from "./store/store";
+import { Provider } from "react-redux";
+// PatternFly utilities
+import "@patternfly/patternfly/utilities/Spacing/spacing.css";
+import "@patternfly/patternfly/utilities/Text/text.css";
+import "@patternfly/patternfly/utilities/Sizing/sizing.css";
+import "@patternfly/patternfly/utilities/Display/display.css";
+import "@patternfly/patternfly/utilities/Accessibility/accessibility.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );

src/store/Identity/users-slice.ts
@@ -1,0 +1,92 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "src/store/store";
+import usersJson from "./users.json";
+// Data types
+import { User } from "src/utils/datatypes/globalDataTypes";
+
+interface UsersState {
+  usersList: User[];
+}
+
+interface ChangeStatusData {
+  newStatus: string;
+  selectedUsers: string[];
+}
+
+const initialState: UsersState = {
+  usersList: usersJson,
+};
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    addUser: (state, action: PayloadAction<User>) => {
+      const newUser = action.payload;
+      state.usersList.push({
+        userId: newUser.userId,
+        userLogin: newUser.userLogin,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        status: newUser.status,
+        uid: newUser.uid,
+        emailAddress: newUser.emailAddress,
+        phone: newUser.phone,
+        jobTitle: newUser.jobTitle,
+      });
+      // Update json file
+    },
+    removeUser: (state, action: PayloadAction<string>) => {
+      const userId = action.payload;
+      const updatedUserList = state.usersList.filter(
+        (user) => user.userId !== userId
+      );
+      // If not empty, replace userList by new array
+      if (updatedUserList) {
+        state.usersList = updatedUserList;
+        // Update json file
+      }
+    },
+    changeStatus: (state, action: PayloadAction<ChangeStatusData>) => {
+      const newStatus =
+        action.payload.newStatus.charAt(0).toUpperCase() +
+        action.payload.newStatus.slice(1) +
+        "d";
+      const selectedUsersIds = action.payload.selectedUsers;
+      let selectedUsersCount = selectedUsersIds.length;
+
+      // Retrieve users
+      for (let i = 0; i < selectedUsersIds.length; i++) {
+        for (let j = 0; j < state.usersList.length; j++) {
+          if (selectedUsersCount > 0) {
+            // Find User by userId
+            if (selectedUsersIds[i] === state.usersList[j].userId) {
+              // Update the status only
+              const updatedUser = {
+                userId: state.usersList[j].userId,
+                userLogin: state.usersList[j].userLogin,
+                firstName: state.usersList[j].firstName,
+                lastName: state.usersList[j].lastName,
+                status: newStatus,
+                uid: state.usersList[j].uid,
+                emailAddress: state.usersList[j].emailAddress,
+                phone: state.usersList[j].phone,
+                jobTitle: state.usersList[j].jobTitle,
+              };
+              // Replace entry
+              state.usersList[j] = updatedUser;
+              selectedUsersCount--;
+            }
+          } else {
+            // When all ocurrences in selectedUsers array are found, nothing else to search
+            break;
+          }
+        }
+      }
+    },
+  },
+});
+
+export const { addUser, removeUser, changeStatus } = usersSlice.actions;
+export const selectUsers = (state: RootState) => state.users.usersList;
+export default usersSlice.reducer;

src/store/hooks.ts
@@ -1,0 +1,6 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

src/store/shared/shared-slice.ts
@@ -1,0 +1,105 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "src/store/store";
+
+/*
+ * This redux slice is intended for shared variables that are being
+ * used by different components throughout the webui app. Instead of
+ * passing the state variables via props to the components, this is
+ * done via redux to reduce the amount of variables passed by the
+ * component structure and make them more lightweight.
+ */
+
+/*
+ * - selectedUsers (string[])
+ *    List of selected users (via bulk selector or checkboxes on table) .
+ * - isDeleteButtonDisabled (boolean)
+ *    Enables / disables the 'Delete' button state.
+ * - isEnableButtonDisabled (boolean)
+ *    Enables / disables the 'Enable' button state.
+ * - isDisableButtonDisabled (boolean)
+ *    Disables / disables the 'Disable' button state.
+ * - isDisableEnableOp (boolean)
+ *    If some entries' status has been updated, unselect selected rows.
+ * - selectedUserIds (string[])
+ *    Selected user ids state.
+ * - selectedPerPage (number)
+ *    Elements selected (per page). This will help to calculate the
+ *    remaining elements on a specific page (bulk selector).
+ * - showTableRows (boolean)
+ *    Flag to indicate if the tables rows should be shown.
+ * - isDeletion (boolean)
+ *    If some entries have been deleted, restore the 'selectedUsers' list.
+ */
+
+interface SharedState {
+  selectedUsers: string[];
+  isDeleteButtonDisabled: boolean;
+  isEnableButtonDisabled: boolean;
+  isDisableButtonDisabled: boolean;
+  isDisableEnableOp: boolean;
+  selectedUserIds: string[];
+  selectedPerPage: number;
+  showTableRows: boolean;
+  isDeletion: boolean;
+}
+
+const initialState: SharedState = {
+  selectedUsers: [],
+  isDeleteButtonDisabled: true,
+  isEnableButtonDisabled: true,
+  isDisableButtonDisabled: true,
+  isDisableEnableOp: false,
+  selectedUserIds: [],
+  selectedPerPage: 0,
+  showTableRows: false,
+  isDeletion: false,
+};
+
+const sharedSlice = createSlice({
+  name: "shared",
+  initialState,
+  reducers: {
+    setSelectedUsers: (state, action: PayloadAction<string[]>) => {
+      state.selectedUsers = action.payload;
+    },
+    setIsDeleteButtonDisabled: (state, action: PayloadAction<boolean>) => {
+      state.isDeleteButtonDisabled = action.payload;
+    },
+    setIsEnableButtonDisabled: (state, action: PayloadAction<boolean>) => {
+      state.isEnableButtonDisabled = action.payload;
+    },
+    setIsDisableButtonDisabled: (state, action: PayloadAction<boolean>) => {
+      state.isDisableButtonDisabled = action.payload;
+    },
+    setIsDisableEnableOp: (state, action: PayloadAction<boolean>) => {
+      state.isDisableEnableOp = action.payload;
+    },
+    setSelectedUserIds: (state, action: PayloadAction<string[]>) => {
+      state.selectedUserIds = action.payload;
+    },
+    setSelectedPerPage: (state, action: PayloadAction<number>) => {
+      state.selectedPerPage = action.payload;
+    },
+    setShowTableRows: (state, action: PayloadAction<boolean>) => {
+      state.showTableRows = action.payload;
+    },
+    setIsDeletion: (state, action: PayloadAction<boolean>) => {
+      state.isDeletion = action.payload;
+    },
+  },
+});
+
+export const {
+  setSelectedUsers,
+  setIsDeleteButtonDisabled,
+  setIsEnableButtonDisabled,
+  setIsDisableButtonDisabled,
+  setIsDisableEnableOp,
+  setSelectedUserIds,
+  setSelectedPerPage,
+  setShowTableRows,
+  setIsDeletion,
+} = sharedSlice.actions;
+export const selectSelectedUsers = (state: RootState) =>
+  state.shared.selectedUsers;
+export default sharedSlice.reducer;

src/utils/datatypes/globalDataTypes.ts
@@ -1,0 +1,11 @@
+export interface User {
+  userId: string;
+  userLogin: string;
+  firstName: string;
+  lastName: string;
+  status: string;
+  uid: string;
+  emailAddress: string;
+  phone: string;
+  jobTitle: string;
+}

src/utils/utils.ts
@@ -1,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+// Data type
+import { User } from "./datatypes/globalDataTypes";
+
+/*
+ * Functions that can be reusable and called by several components throughout the application.
+ */
+
+// Helper method: Given an users list and status, check if some entry has different status
+export const checkEqualStatus = (status: string, usersList: User[]) => {
+  const usersWithOtherStatus = usersList.filter(
+    (user) => user.status !== status
+  );
+  return usersWithOtherStatus.length === 0;
+};
+
+// Determine whether a user is selectable or not
+export const isUserSelectable = (user: User) => user.userLogin !== "";

commit 5656edc291613709c0dcab8360b6c2d706c6a5c7

Fix comments on 'store.ts'
The commmented leftover text on `store.ts` needs to
be removed as part of the fix.

Fixes: 1ba0ad0c2634e72cbb3c5c464767146f1a2dda7f
Signed-off-by: Carla Martinez <carlmart@redhat.com>

src/store/store.ts
@@ -1,21 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./Identity/users-slice";
 import sharedReducer from "./shared/shared-slice";
-// import userGroupsReducer from "./Identity/userGroups-slice";
-// import netgroupsReducer from "./Identity/netgroups-slice";
-// import rolesReducer from "./IPA server/roles-slice";
-// import hbacRulesReducer from "./Policy/hbacRules-slice";
-// import sudoRulesReducer from "./Policy/sudoRules-slice";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     shared: sharedReducer,
-    // usergroups: userGroupsReducer,
-    // netgroups: netgroupsReducer,
-    // roles: rolesReducer,
-    // hbacrules: hbacRulesReducer,
-    // sudorules: sudoRulesReducer,
   },
 });
 