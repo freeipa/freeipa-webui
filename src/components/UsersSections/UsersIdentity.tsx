f8204b9e9c9b187183683140272336e6a313bf92
Carla Martinez <carlmart@redhat.com>
2022-11-10 16:42:10 +0100

    Create the user section elements
    In order to show the user data, the different sections need
    to be created according to the given mockups and the existing
    FreeIPA WebUI as a reference for functionality.
    
    The following sections have been created:
    - 'Identity settings' (`UsersIdentity`)
    - 'Account settings' (`UsersAccountSettings`)
    - 'Password policy' (`UsersPasswordPolicy`)
    - 'Kerberos ticket' (`UsersKerberosTicket`)
    - 'Contact settings' (`UsersContactSettings`)
    - 'Mailing address' (`UsersMailingAddress`)
    - 'Employee information' (`UsersEmployeeInfo`)
    - 'User attributes for SMB services' (`UsersAttributesSMB`)
    
    Apart from that, the `CertificateMappingDataModal` component
    has been created to render the modal of the `Certificate
    mapping data` option (located in the `Account settings` section).
    
    Also, the functionality of the action buttons located in the
    fields (if any) have been implemented. That allows to dinamically
    add new elements such as texboxes or textareas. This functionality
    is not include the action buttons located in the very bottom of
    the page (`Refresh`,`Revert`, `Save`, and the kebab selector options),
    as this will be covered in a different task.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/components/UsersSections/UsersIdentity.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+// PatternFly
+import {
+  Flex,
+  FlexItem,
+  Form,
+  FormGroup,
+  TextInput,
+} from "@patternfly/react-core";
+// Data types
+import { User } from "src/utils/datatypes/globalDataTypes";
+
+interface PropsToUsersIdentity {
+  user: User;
+}
+
+const UsersIdentity = (props: PropsToUsersIdentity) => {
+  // TODO: This state variables should update the user data via the IPA API (`user_mod`)
+  const [firstName] = useState(props.user.firstName);
+  const [lastName, setLastName] = useState(props.user.lastName);
+  const [fullName, setFullName] = useState(firstName + " " + lastName);
+  const [jobTitle] = useState(props.user.jobTitle);
+  const [gecos, setGecos] = useState(fullName);
+  const [classField, setClassField] = useState("");
+
+  const lastNameInputHandler = (value: string) => {
+    setLastName(value);
+  };
+
+  const fullNameInputHandler = (value: string) => {
+    setFullName(value);
+  };
+
+  const gecosInputHandler = (value: string) => {
+    setGecos(value);
+  };
+
+  const classFieldInputHandler = (value: string) => {
+    setClassField(value);
+  };
+
+  return (
+    <Flex direction={{ default: "column", lg: "row" }}>
+      <FlexItem flex={{ default: "flex_1" }}>
+        <Form className="pf-u-mb-lg">
+          <FormGroup label="First name" fieldId="first-name" isRequired>
+            <TextInput
+              id="first-name"
+              name="givenname"
+              value={firstName}
+              type="text"
+              aria-label="first name"
+              isRequired
+            />
+          </FormGroup>
+          <FormGroup label="Last name" fieldId="last-name" isRequired>
+            <TextInput
+              id="last-name"
+              name="sn"
+              value={lastName}
+              type="text"
+              onChange={lastNameInputHandler}
+              aria-label="last name"
+              isRequired
+            />
+          </FormGroup>
+          <FormGroup label="Full name" fieldId="full-name" isRequired>
+            <TextInput
+              id="full-name"
+              name="cn"
+              value={fullName}
+              type="text"
+              onChange={fullNameInputHandler}
+              aria-label="full name"
+              isRequired
+            />
+          </FormGroup>
+        </Form>
+      </FlexItem>
+      <FlexItem flex={{ default: "flex_1" }}>
+        <Form className="pf-u-mb-lg">
+          <FormGroup label="Job title" fieldId="job-title">
+            <TextInput
+              id="job-title"
+              name="title"
+              value={jobTitle}
+              type="text"
+              aria-label="job title"
+              isDisabled
+            />
+          </FormGroup>
+          <FormGroup label="GECOS" fieldId="gecos">
+            <TextInput
+              id="gecos"
+              name="gecos"
+              value={gecos}
+              type="text"
+              onChange={gecosInputHandler}
+              aria-label="gecos"
+              isDisabled
+            />
+          </FormGroup>
+          <FormGroup label="Class" fieldId="class-field">
+            <TextInput
+              id="class-field"
+              name="userclass"
+              value={classField}
+              type="text"
+              onChange={classFieldInputHandler}
+              aria-label="class field"
+              isRequired
+            />
+          </FormGroup>
+        </Form>
+      </FlexItem>
+    </Flex>
+  );
+};
+
+export default UsersIdentity;
---
+++ src/components/modals/CertificateMappingDataModal.tsx
@@ -0,0 +1,258 @@
+import React from "react";
+// PatternFly
+import {
+  Flex,
+  FlexItem,
+  Form,
+  FormGroup,
+  Modal,
+  Radio,
+  TextArea,
+  TextInput,
+} from "@patternfly/react-core";
+// Layouts
+import SecondaryButton from "../layouts/SecondaryButton";
+import PopoverWithIconLayout from "../layouts/PopoverWithIconLayout";
+
+// Generic data to pass to the Textbox adder
+interface ElementData {
+  id: string | number;
+  element: string;
+}
+
+interface PropsToCertificateMappingData {
+  // Modal options
+  isOpen: boolean;
+  onClose: () => void;
+  actions: JSX.Element[];
+  // First radio option: Certificate mapping data
+  isCertMappingDataChecked: boolean;
+  onChangeCertMappingDataCheck: (value: boolean) => void;
+  // Second radio option: Issuer and subject
+  isIssuerAndSubjectChecked: boolean;
+  onChangeIssuerAndSubjectCheck: (value: boolean) => void;
+  issuerValue: string;
+  subjectValue: string;
+  onChangeIssuer: (value: string) => void;
+  onChangeSubject: (value: string) => void;
+  // Generated texboxes, textareas, and data
+  certificateMappingDataList: ElementData[];
+  certificateList: ElementData[];
+  onAddCertificateMappingDataHandler: () => void;
+  onHandleCertificateMappingDataChange: (
+    value: string,
+    event: React.FormEvent<HTMLInputElement>,
+    idx: number
+  ) => void;
+  onRemoveCertificateMappingDataHandler: (idx: number) => void;
+  onAddCertificateHandler: () => void;
+  onHandleCertificateChange: (
+    value: string,
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+    idx: number
+  ) => void;
+  onRemoveCertificateHandler: (idx: number) => void;
+}
+
+const CertificateMappingDataModal = (props: PropsToCertificateMappingData) => {
+  // Popover messages
+  const certificateMessage = (
+    <div>
+      <p>Base-64 encoder user certificate</p>
+    </div>
+  );
+  const issuerMessage = (
+    <div>
+      <p>Issuer of the certificate</p>
+    </div>
+  );
+  const subjectMessage = (
+    <div>
+      <p>Subject of the certificate</p>
+    </div>
+  );
+
+  return (
+    <Modal
+      variant="small"
+      title="Add certificate mapping data"
+      isOpen={props.isOpen}
+      onClose={props.onClose}
+      actions={props.actions}
+    >
+      <Radio
+        isChecked={props.isCertMappingDataChecked}
+        name="cert-mapping-data-radio"
+        onChange={props.onChangeCertMappingDataCheck}
+        label="Certificate mapping data"
+        id="certificate-mapping-data"
+        className="pf-u-mb-md"
+      />
+      <div className="pf-u-ml-lg pf-u-mb-md">
+        <Form>
+          <FormGroup
+            label="Certificate mapping data"
+            fieldId="certificate-mapping-data-modal"
+          >
+            <Flex direction={{ default: "column" }} name="ipacertmapdata">
+              {props.certificateMappingDataList.map((certMap, idx) => (
+                <Flex
+                  direction={{ default: "row" }}
+                  key={certMap.id + "-" + idx + "-div"}
+                  name="value"
+                >
+                  <FlexItem
+                    key={certMap.id + "-textbox"}
+                    flex={{ default: "flex_1" }}
+                  >
+                    <TextInput
+                      id="cert-map-data"
+                      value={certMap.element}
+                      type="text"
+                      name={"ipacertmapdata-" + idx}
+                      aria-label="certificate mapping data textbox"
+                      onChange={(value, event) =>
+                        props.onHandleCertificateMappingDataChange(
+                          value,
+                          event,
+                          idx
+                        )
+                      }
+                    />
+                  </FlexItem>
+                  <FlexItem key={certMap.id + "-delete-button"}>
+                    <SecondaryButton
+                      name="remove"
+                      onClickHandler={() =>
+                        props.onRemoveCertificateMappingDataHandler(idx)
+                      }
+                    >
+                      Delete
+                    </SecondaryButton>
+                  </FlexItem>
+                </Flex>
+              ))}
+            </Flex>
+            <SecondaryButton
+              classname="pf-u-mt-sm pf-u-mb-0"
+              isDisabled={!props.isCertMappingDataChecked}
+              onClickHandler={props.onAddCertificateMappingDataHandler}
+            >
+              Add
+            </SecondaryButton>
+          </FormGroup>
+          <FormGroup
+            label="Certificate"
+            fieldId="certificate"
+            labelIcon={
+              <PopoverWithIconLayout
+                message={certificateMessage}
+                hasAutoWidth={true}
+              />
+            }
+          >
+            <Flex direction={{ default: "column" }} name="certificate">
+              {props.certificateList.map((certificate, idx) => (
+                <Flex
+                  direction={{ default: "row" }}
+                  key={certificate.id + "-" + idx + "-div"}
+                  alignItems={{ default: "alignItemsFlexEnd" }}
+                  name="value"
+                >
+                  <FlexItem
+                    key={certificate.id + "-textbox"}
+                    flex={{ default: "flex_1" }}
+                  >
+                    <TextArea
+                      id="cert-map-data"
+                      value={certificate.element}
+                      type="text"
+                      name={"certificate-" + idx}
+                      aria-label="certificate textarea"
+                      resizeOrientation="vertical"
+                      onChange={(value, event) =>
+                        props.onHandleCertificateChange(value, event, idx)
+                      }
+                      style={{ height: "135px" }}
+                    />
+                  </FlexItem>
+                  <FlexItem key={certificate.id + "-delete-button"}>
+                    <SecondaryButton
+                      name="remove"
+                      onClickHandler={() =>
+                        props.onRemoveCertificateHandler(idx)
+                      }
+                    >
+                      Delete
+                    </SecondaryButton>
+                  </FlexItem>
+                </Flex>
+              ))}
+            </Flex>
+            <SecondaryButton
+              classname="pf-u-mt-sm"
+              isDisabled={!props.isCertMappingDataChecked}
+              onClickHandler={props.onAddCertificateHandler}
+            >
+              Add
+            </SecondaryButton>
+          </FormGroup>
+        </Form>
+      </div>
+      <Radio
+        isChecked={props.isIssuerAndSubjectChecked}
+        name="issuer-and-subject-radio"
+        onChange={props.onChangeIssuerAndSubjectCheck}
+        label="Issuer and subject"
+        id="issuer-and-subject"
+        className="pf-u-mb-md"
+      />
+      <div className="pf-u-ml-lg pf-u-mb-md">
+        <Form>
+          <FormGroup
+            label="Issuer"
+            fieldId="issuer"
+            labelIcon={
+              <PopoverWithIconLayout
+                message={issuerMessage}
+                hasAutoWidth={true}
+              />
+            }
+          >
+            <TextInput
+              id="issuer-textbox"
+              value={props.issuerValue}
+              type="text"
+              name="issuer"
+              aria-label="issuer textbox"
+              onChange={props.onChangeIssuer}
+              isDisabled={!props.isIssuerAndSubjectChecked}
+            />
+          </FormGroup>
+          <FormGroup
+            label="Subject"
+            fieldId="subject"
+            labelIcon={
+              <PopoverWithIconLayout
+                message={subjectMessage}
+                hasAutoWidth={true}
+              />
+            }
+          >
+            <TextInput
+              id="subject-textbox"
+              value={props.subjectValue}
+              type="text"
+              name="subject"
+              aria-label="subject textbox"
+              onChange={props.onChangeSubject}
+              isDisabled={!props.isIssuerAndSubjectChecked}
+            />
+          </FormGroup>
+        </Form>
+      </div>
+    </Modal>
+  );
+};
+
+export default CertificateMappingDataModal;
