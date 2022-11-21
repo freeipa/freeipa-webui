a71d047343c37f9ac994cbd63063b34aa647fd10
Carla Martinez <carlmart@redhat.com>
2022-11-21 09:59:36 +0100

    Define initial data for the tab groups
    The 'Is a member of' section provides a way to manage
    the groups to which a specific user can be member of.
    These types are known as 'member groups'.
    
    There are five different types of member groups whose
    entity can be defined in different pages / sections
    alongside the FreeIPA page:
    - User groups (`Identity`)
    - Netgroups (`Identity`)
    - Roles (`IPA server`)
    - HBAC rules (`Policy`)
    - Sudo rules (`Policy`)
    
    To prepare the data for being used by Redux, this has
    been initialized as follows:
    - The data types are being defined in the `globalDataTypes.ts`
    file.
    - Assuming that the data in the tables is not empty in this
    specific solution, the initial data values have been defined
    in the `GroupRepositories.ts` file.
    - The data that will be shown when assigning a new member
    group to the user is defined in the following files:
         - netgroups.json
         - userGroups.json
         - roles.json
         - hbacRules.json
         - sudoRules.json
    
    This data will be used by Redux to manage all the initial
    states by default.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/utils/datatypes/globalDataTypes.ts
+++ src/utils/datatypes/globalDataTypes.ts
@@ -9,3 +9,31 @@ export interface User {
   phone: string;
   jobTitle: string;
 }
+
+export interface UserGroup {
+  name: string;
+  gid: string;
+  description: string;
+}
+
+export interface Netgroup {
+  name: string;
+  description: string;
+}
+
+export interface Roles {
+  name: string;
+  description: string;
+}
+
+export interface HBACRules {
+  name: string;
+  status: string;
+  description: string;
+}
+
+export interface SudoRules {
+  name: string;
+  status: string;
+  description: string;
+}
