9b888b193f8806a91e4a62b3cb7a81b22330b1de
Carla Martinez <carlmart@redhat.com>
2022-11-21 09:59:36 +0100

    Define initial data for the tab groups
    The 'Is a member of' section provides a way to manage
    the groups to which a specific user can be member of.
    These types are known as 'member groups'.
    
    There are five different types of member groups whose
    entity can be defined in different pages / sections
    alongside the FreeIPA page:
    - User groups (`Identity`)
    - Netgroups (`Identity`)
    - Roles (`IPA server`)
    - HBAC rules (`Policy`)
    - Sudo rules (`Policy`)
    
    To prepare the data for being used by Redux, this has
    been initialized as follows:
    - The data types are being defined in the `globalDataTypes.ts`
    file.
    - Assuming that the data in the tables is not empty in this
    specific solution, the initial data values have been defined
    in the `GroupRepositories.ts` file.
    - The data that will be shown when assigning a new member
    group to the user is defined in the following files:
         - netgroups.json
         - userGroups.json
         - roles.json
         - hbacRules.json
         - sudoRules.json
    
    This data will be used by Redux to manage all the initial
    states by default.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/utils/data/GroupRepositories.ts
@@ -0,0 +1,123 @@
+/* eslint-disable prefer-const */
+
+/*
+ * This is a temporal file to hold the repositories (aka. the intial data lists)
+ * used on the 'Active users' page > 'Is a member of' section.
+ *
+ * In the future, this must be retrieved from other source (eg.: API call, Redux, etc)
+ */
+
+import {
+  UserGroup,
+  Netgroup,
+  Roles,
+  HBACRules,
+  SudoRules,
+} from "../datatypes/globalDataTypes";
+
+// 'User groups' initial data
+export let userGroupsInitialData: UserGroup[] = [
+  {
+    name: "Initial admins",
+    gid: "12345678",
+    description: "This is a description for initial admins",
+  },
+  {
+    name: "Other admins",
+    gid: "234456567",
+    description: "This is a description for other admins",
+  },
+  {
+    name: "Other admins 1",
+    gid: "234456567",
+    description: "This is a description for other admins 1",
+  },
+  {
+    name: "Other admins 2",
+    gid: "234456567",
+    description: "This is a description for other admins 2",
+  },
+  {
+    name: "Other admins 3",
+    gid: "234456567",
+    description: "This is a description for other admins 3",
+  },
+  {
+    name: "Other admins 4",
+    gid: "234456567",
+    description: "This is a description for other admins 4",
+  },
+  {
+    name: "Other admins 5",
+    gid: "234456567",
+    description: "This is a description for other admins 5",
+  },
+  {
+    name: "Other admins 6",
+    gid: "234456567",
+    description: "This is a description for other admins 6",
+  },
+  {
+    name: "Other admins 7",
+    gid: "234456567",
+    description: "This is a description for other admins 7",
+  },
+  {
+    name: "Other admins 8",
+    gid: "234456567",
+    description: "This is a description for other admins 8",
+  },
+  {
+    name: "Other admins 9",
+    gid: "234456567",
+    description: "This is a description for other admins 9",
+  },
+  {
+    name: "Other admins 10",
+    gid: "234456567",
+    description: "This is a description for other admins 10",
+  },
+  {
+    name: "Other admins 11",
+    gid: "234456567",
+    description: "This is a description for other admins 11",
+  },
+  {
+    name: "Other admins 12",
+    gid: "234456567",
+    description: "This is a description for other admins 12",
+  },
+];
+
+// 'Netgroups' initial data
+export let netgroupsInitialData: Netgroup[] = [
+  {
+    name: "netgroup1",
+    description: "First netgroup",
+  },
+  {
+    name: "netgroup2",
+    description: "Second netgroup",
+  },
+];
+
+// 'Roles' initial data
+export let rolesInitialData: Roles[] = [];
+
+// 'HBAC rules' initial data
+export let hbacRulesInitialData: HBACRules[] = [
+  {
+    name: "allow_all",
+    status: "Enabled",
+    description: "Allow all users to access any host from any host",
+  },
+  {
+    name: "allow_systemd-user",
+    status: "Enabled",
+    description:
+      "Allow pam_systemd to run user@.service to create a system user session",
+  },
+];
+
+// 'Sudo rules' initial data
+export let sudoRulesInitialData: SudoRules[] = [];
