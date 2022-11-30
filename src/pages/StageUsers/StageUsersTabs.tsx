d9fa240817d13f75967fb9fe4584ba2f305f67af
Carla Martinez <carlmart@redhat.com>
2022-11-30 11:38:08 +0100

    Add 'Stage users' page and 'Settings' section
    The `StageUsers` component has been added to hold the
    main functionality of the 'Stage users' page.
    
    Related to this, the 'Settings' section has been
    added to this page as required. This is done via
    the `StageUsersTabs` component.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/pages/StageUsers/StageUsersTabs.tsx
+++ src/pages/StageUsers/StageUsersTabs.tsx
@@ -1,9 +1,85 @@
-import React from "react";
-// React router dom
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// PatternFly
+import {
+  Title,
+  Page,
+  PageSection,
+  PageSectionVariants,
+  TextContent,
+  Text,
+  Tabs,
+  Tab,
+  TabTitleText,
+} from "@patternfly/react-core";
+// React Router DOM
+import { useLocation } from "react-router-dom";
+// Navigation
+import { URL_PREFIX } from "src/navigation/NavRoutes";
+// Data types
+import { User } from "src/utils/datatypes/globalDataTypes";
+// Other
+import UserSettings from "src/components/UserSettings";
+// Layouts
+import BreadcrumbLayout from "src/components/layouts/BreadcrumbLayout";
 
 const StageUsersTabs = () => {
-  return <Link to="third-level-page-stage">This is Stage users tabs!</Link>;
+  // Get location (React Router DOM) and get state data
+  const location = useLocation();
+  const userData: User = location.state as User;
+
+  // Tab
+  const [activeTabKey, setActiveTabKey] = useState(0);
+
+  const handleTabClick = (
+    _event: React.MouseEvent<HTMLElement, MouseEvent>,
+    tabIndex: number | string
+  ) => {
+    setActiveTabKey(tabIndex as number);
+  };
+
+  // 'pagesVisited' array will contain the visited pages.
+  // - Those will be passed to the BreadcrumbLayout component.
+  const pagesVisited = [
+    {
+      name: "Stage users",
+      url: URL_PREFIX + "/stage-users",
+    },
+  ];
+
+  return (
+    <Page>
+      <PageSection variant={PageSectionVariants.light} className="pf-u-pr-0">
+        <BreadcrumbLayout
+          className="pf-u-mb-md"
+          userId={userData.userId}
+          pagesVisited={pagesVisited}
+        />
+        <TextContent>
+          <Title headingLevel="h1">
+            <Text>{userData.userId}</Text>
+          </Title>
+        </TextContent>
+      </PageSection>
+      <PageSection type="tabs" variant={PageSectionVariants.light} isFilled>
+        <Tabs
+          activeKey={activeTabKey}
+          onSelect={handleTabClick}
+          variant="light300"
+          isBox
+          className="pf-u-ml-lg"
+        >
+          <Tab
+            eventKey={0}
+            name="details"
+            title={<TabTitleText>Settings</TabTitleText>}
+          >
+            <PageSection className="pf-u-pb-0"></PageSection>
+            <UserSettings user={userData} from="stage-users" />
+          </Tab>
+        </Tabs>
+      </PageSection>
+    </Page>
+  );
 };
 
 export default StageUsersTabs;
