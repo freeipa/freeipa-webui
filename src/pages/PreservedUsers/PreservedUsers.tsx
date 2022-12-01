9261fab76f8a43162384d6cbcb8030b2f1606764
Carla Martinez <carlmart@redhat.com>
2022-12-01 10:44:24 +0100

    Add main page and 'Settings' section
    The `PreservedUsers` component has been added to hold the
    main functionality of the 'Preserved users' page.
    
    Related to this, the 'Settings' section has been
    added to this page as required. This is done via
    the `PreservedUsersTabs` component.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/pages/PreservedUsers/PreservedUsers.tsx
+++ src/pages/PreservedUsers/PreservedUsers.tsx
@@ -1,7 +1,477 @@
-import React from "react";
+import React, { useState } from "react";
+// PatternFly
+import {
+  DropdownItem,
+  Page,
+  PageSection,
+  PageSectionVariants,
+  SearchInput,
+  TextVariants,
+  PaginationVariant,
+} from "@patternfly/react-core";
+// PatternFly table
+import {
+  InnerScrollContainer,
+  OuterScrollContainer,
+} from "@patternfly/react-table";
+// Icons
+import OutlinedQuestionCircleIcon from "@patternfly/react-icons/dist/esm/icons/outlined-question-circle-icon";
+// Data types
+import { User } from "src/utils/datatypes/globalDataTypes";
+import { ToolbarItem } from "src/components/layouts/ToolbarLayout";
+// Redux
+import { useAppSelector } from "src/store/hooks";
+// Layouts
+import TitleLayout from "src/components/layouts/TitleLayout";
+import HelpTextWithIconLayout from "src/components/layouts/HelpTextWithIconLayout";
+import KebabLayout from "src/components/layouts/KebabLayout";
+import SecondaryButton from "src/components/layouts/SecondaryButton";
+import ToolbarLayout from "src/components/layouts/ToolbarLayout";
+// Tables
+import UsersTable from "../../components/tables/UsersTable";
+// Components
+import PaginationPrep from "src/components/PaginationPrep";
+import BulkSelectorPrep from "src/components/BulkSelectorPrep";
+// Modals
+import AddUser from "src/components/modals/AddUser";
+import DeleteUsers from "src/components/modals/DeleteUsers";
+import DisableEnableUsers from "src/components/modals/DisableEnableUsers";
+// Utils
+import { isUserSelectable } from "src/utils/utils";
 
 const PreservedUsers = () => {
-  return <p>Preserved Users Page</p>;
+  // Initialize preserved users list (Redux)
+  const preservedUsersList = useAppSelector(
+    (state) => state.preservedUsers.usersList
+  );
+
+  // Selected users state
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+
+  const updateSelectedUsers = (newSelectedUsers: string[]) => {
+    setSelectedUsers(newSelectedUsers);
+  };
+
+  // 'Delete' button state
+  const [isDeleteButtonDisabled, setIsDeleteButtonDisabled] =
+    useState<boolean>(true);
+
+  const updateIsDeleteButtonDisabled = (value: boolean) => {
+    setIsDeleteButtonDisabled(value);
+  };
+
+  // If some entries have been deleted, restore the selectedUsers list
+  const [isDeletion, setIsDeletion] = useState(false);
+
+  const updateIsDeletion = (value: boolean) => {
+    setIsDeletion(value);
+  };
+
+  // 'Enable' button state
+  const [isEnableButtonDisabled, setIsEnableButtonDisabled] =
+    useState<boolean>(true);
+
+  const updateIsEnableButtonDisabled = (value: boolean) => {
+    setIsEnableButtonDisabled(value);
+  };
+
+  // 'Disable' button state
+  const [isDisableButtonDisabled, setIsDisableButtonDisabled] =
+    useState<boolean>(true);
+
+  const updateIsDisableButtonDisabled = (value: boolean) => {
+    setIsDisableButtonDisabled(value);
+  };
+
+  // If some entries' status has been updated, unselect selected rows
+  const [isDisableEnableOp, setIsDisableEnableOp] = useState(false);
+
+  const updateIsDisableEnableOp = (value: boolean) => {
+    setIsDisableEnableOp(value);
+  };
+
+  // - Selected user ids state
+  const [selectedUserIds, setSelectedUserIds] = useState<string[]>([]);
+
+  const updateSelectedUserIds = (newSelectedUserIds: string[]) => {
+    setSelectedUserIds(newSelectedUserIds);
+  };
+
+  // Elements selected (per page)
+  //  - This will help to calculate the remaining elements on a specific page (bulk selector)
+  const [selectedPerPage, setSelectedPerPage] = useState<number>(0);
+
+  const updateSelectedPerPage = (selected: number) => {
+    setSelectedPerPage(selected);
+  };
+
+  // Pagination
+  const [page, setPage] = useState<number>(1);
+
+  const updatePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const [perPage, setPerPage] = useState<number>(15);
+
+  const updatePerPage = (newSetPerPage: number) => {
+    setPerPage(newSetPerPage);
+  };
+
+  // Users displayed on the first page
+  const [shownUsersList, setShownUsersList] = useState(
+    preservedUsersList.slice(0, perPage)
+  );
+
+  const updateShownUsersList = (newShownUsersList: User[]) => {
+    setShownUsersList(newShownUsersList);
+  };
+
+  // Show table rows
+  const [showTableRows, setShowTableRows] = useState(false);
+
+  const updateShowTableRows = (value: boolean) => {
+    setShowTableRows(value);
+  };
+
+  // Dropdown kebab
+  const [kebabIsOpen, setKebabIsOpen] = useState(false);
+
+  const dropdownItems = [
+    <DropdownItem key="action" component="button">
+      Rebuild auto membership
+    </DropdownItem>,
+  ];
+
+  const onKebabToggle = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    event: any,
+    isOpen: boolean
+  ) => {
+    setKebabIsOpen(isOpen);
+  };
+
+  const onDropdownSelect = (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    event?: React.SyntheticEvent<HTMLDivElement, Event> | undefined
+  ) => {
+    setKebabIsOpen(!kebabIsOpen);
+  };
+
+  // Modals functionality
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showEnableDisableModal, setShowEnableDisableModal] = useState(false);
+  const [enableDisableOptionSelected, setEnableDisableOptionSelected] =
+    useState("");
+  const onAddClickHandler = () => {
+    setShowAddModal(true);
+  };
+  const onAddModalToggle = () => {
+    setShowAddModal(!showAddModal);
+  };
+
+  const onDeleteHandler = () => {
+    setShowDeleteModal(true);
+  };
+  const onDeleteModalToggle = () => {
+    setShowDeleteModal(!showDeleteModal);
+  };
+
+  const onEnableDisableHandler = (optionClicked: string) => {
+    setIsDeleteButtonDisabled(true); // prevents 'Delete' button to be enabled
+    setIsEnableButtonDisabled(true); // prevents 'Enable' button to be enabled
+    setIsDisableButtonDisabled(true); // prevents 'Disable' button to be enabled
+    setEnableDisableOptionSelected(optionClicked);
+    setShowEnableDisableModal(true);
+  };
+
+  const onEnableDisableModalToggle = () => {
+    setIsEnableButtonDisabled(true); // prevents 'Enable' button to be enabled
+    setIsDisableButtonDisabled(true); // prevents 'Disable' button to be enabled
+    setShowEnableDisableModal(!showEnableDisableModal);
+  };
+
+  // Table-related shared functionality
+  // - Selectable checkboxes on table
+  const selectableUsersTable = preservedUsersList.filter(isUserSelectable); // elements per Table
+
+  // - Selected rows are tracked. Primary key: userLogin
+  const [selectedUserNames, setSelectedUserNames] = useState<string[]>([]);
+
+  const changeSelectedUserNames = (selectedUsernames: string[]) => {
+    setSelectedUserNames(selectedUsernames);
+  };
+
+  // - Helper method to set the selected users from the table
+  const setUserSelected = (user: User, isSelecting = true) =>
+    setSelectedUserNames((prevSelected) => {
+      const otherSelectedUserNames = prevSelected.filter(
+        (r) => r !== user.userLogin
+      );
+      return isSelecting && isUserSelectable(user)
+        ? [...otherSelectedUserNames, user.userLogin]
+        : otherSelectedUserNames;
+    });
+
+  // Data wrappers
+  // - 'PaginationPrep'
+  const paginationData = {
+    page,
+    perPage,
+    updatePage,
+    updatePerPage,
+    showTableRows,
+    updateShowTableRows,
+    updateSelectedPerPage,
+    updateShownUsersList,
+  };
+
+  // - 'BulkSelectorPrep'
+  const usersData = {
+    selectedUsers,
+    updateSelectedUsers,
+    updateSelectedUserIds,
+    selectedUserNames,
+    changeSelectedUserNames,
+    selectableUsersTable,
+    isUserSelectable,
+  };
+
+  const buttonsData = {
+    // isDeleteButtonDisabled, //
+    updateIsDeleteButtonDisabled,
+    // isEnableButtonDisabled, //
+    updateIsEnableButtonDisabled,
+    updateIsDisableButtonDisabled,
+    updateIsDisableEnableOp,
+  };
+
+  const selectedPerPageData = {
+    selectedPerPage,
+    updateSelectedPerPage,
+  };
+
+  // 'DeleteUsers'
+  const deleteUsersButtonsData = {
+    updateIsDeleteButtonDisabled,
+    updateIsDeletion,
+  };
+
+  const selectedUsersData = {
+    selectedUsers,
+    updateSelectedUsers,
+  };
+
+  // 'DisableEnableUsers'
+  const disableEnableButtonsData = {
+    updateIsEnableButtonDisabled,
+    updateIsDisableButtonDisabled,
+    updateIsDisableEnableOp,
+  };
+
+  // 'UsersTable'
+  const usersTableData = {
+    isUserSelectable,
+    selectedUserNames,
+    changeSelectedUserNames,
+    selectedUserIds,
+    updateSelectedUserIds,
+    selectableUsersTable,
+    setUserSelected,
+    updateSelectedUsers,
+  };
+
+  const usersTableButtonsData = {
+    updateIsDeleteButtonDisabled,
+    updateIsEnableButtonDisabled,
+    updateIsDisableButtonDisabled,
+    isDeletion,
+    updateIsDeletion,
+    isDisableEnableOp,
+    updateIsDisableEnableOp,
+  };
+
+  // List of Toolbar items
+  const toolbarItems: ToolbarItem[] = [
+    {
+      key: 0,
+      element: (
+        <BulkSelectorPrep
+          list={preservedUsersList}
+          shownElementsList={shownUsersList}
+          usersData={usersData}
+          buttonsData={buttonsData}
+          selectedPerPageData={selectedPerPageData}
+        />
+      ),
+    },
+    {
+      key: 1,
+      element: <SearchInput aria-label="Search user" placeholder="Search" />,
+      toolbarItemVariant: "search-filter",
+      toolbarItemSpacer: { default: "spacerMd" },
+    },
+    {
+      key: 2,
+      toolbarItemVariant: "separator",
+    },
+    {
+      key: 3,
+      element: <SecondaryButton>Refresh</SecondaryButton>,
+    },
+    {
+      key: 4,
+      element: (
+        <SecondaryButton
+          isDisabled={isDeleteButtonDisabled}
+          onClickHandler={onDeleteHandler}
+        >
+          Delete
+        </SecondaryButton>
+      ),
+    },
+    {
+      key: 5,
+      element: (
+        <SecondaryButton onClickHandler={onAddClickHandler}>
+          Add
+        </SecondaryButton>
+      ),
+    },
+    {
+      key: 6,
+      element: (
+        <SecondaryButton
+          isDisabled={isDisableButtonDisabled}
+          onClickHandler={() => onEnableDisableHandler("disable")}
+        >
+          Disable
+        </SecondaryButton>
+      ),
+    },
+    {
+      key: 7,
+      element: (
+        <SecondaryButton
+          isDisabled={isEnableButtonDisabled}
+          onClickHandler={() => onEnableDisableHandler("enable")}
+        >
+          Enable
+        </SecondaryButton>
+      ),
+    },
+    {
+      key: 8,
+      element: (
+        <KebabLayout
+          onDropdownSelect={onDropdownSelect}
+          onKebabToggle={onKebabToggle}
+          idKebab="main-dropdown-kebab"
+          isKebabOpen={kebabIsOpen}
+          isPlain={true}
+          dropdownItems={dropdownItems}
+        />
+      ),
+    },
+    {
+      key: 9,
+      toolbarItemVariant: "separator",
+    },
+    {
+      key: 10,
+      element: (
+        <HelpTextWithIconLayout
+          textComponent={TextVariants.p}
+          subTextComponent={TextVariants.a}
+          subTextIsVisitedLink={true}
+          textContent="Help"
+          icon={
+            <OutlinedQuestionCircleIcon className="pf-u-primary-color-100 pf-u-mr-sm" />
+          }
+        />
+      ),
+    },
+    {
+      key: 11,
+      element: (
+        <PaginationPrep
+          list={preservedUsersList}
+          paginationData={paginationData}
+          widgetId="pagination-options-menu-top"
+          isCompact={true}
+        />
+      ),
+      toolbarItemAlignment: { default: "alignRight" },
+    },
+  ];
+
+  // Render 'Preserved users'
+  return (
+    <Page>
+      <PageSection variant={PageSectionVariants.light}>
+        <TitleLayout
+          id="preserved users title"
+          headingLevel="h1"
+          text="Preserved users"
+        />
+      </PageSection>
+      <PageSection
+        variant={PageSectionVariants.light}
+        isFilled={false}
+        className="pf-u-m-lg pf-u-pb-md pf-u-pl-0 pf-u-pr-0"
+      >
+        <ToolbarLayout
+          className="pf-u-pt-0 pf-u-pl-lg pf-u-pr-md"
+          contentClassName="pf-u-p-0"
+          toolbarItems={toolbarItems}
+        />
+        <div style={{ height: `calc(100vh - 352.2px)` }}>
+          <OuterScrollContainer>
+            <InnerScrollContainer>
+              <UsersTable
+                elementsList={preservedUsersList}
+                shownElementsList={shownUsersList}
+                from="preserved-users"
+                showTableRows={showTableRows}
+                usersData={usersTableData}
+                buttonsData={usersTableButtonsData}
+                paginationData={selectedPerPageData}
+              />
+            </InnerScrollContainer>
+          </OuterScrollContainer>
+        </div>
+        <PaginationPrep
+          list={preservedUsersList}
+          paginationData={paginationData}
+          variant={PaginationVariant.bottom}
+          widgetId="pagination-options-menu-bottom"
+          perPageComponent="button"
+          className="pf-u-pb-0 pf-u-pr-md"
+        />
+      </PageSection>
+      <AddUser
+        show={showAddModal}
+        from="preserved-users"
+        handleModalToggle={onAddModalToggle}
+      />
+      <DeleteUsers
+        show={showDeleteModal}
+        from="preserved-users"
+        handleModalToggle={onDeleteModalToggle}
+        selectedUsersData={selectedUsersData}
+        buttonsData={deleteUsersButtonsData}
+      />
+      <DisableEnableUsers
+        show={showEnableDisableModal}
+        from="preserved-users"
+        handleModalToggle={onEnableDisableModalToggle}
+        optionSelected={enableDisableOptionSelected}
+        selectedUsersData={selectedUsersData}
+        buttonsData={disableEnableButtonsData}
+      />
+    </Page>
+  );
 };
 
 export default PreservedUsers;
