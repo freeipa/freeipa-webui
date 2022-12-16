fb58d9841840194e98ff3976e5a515e72248178b
Carla Martinez <carlmart@redhat.com>
2022-12-16 11:54:11 +0100

    Add action buttons functionality
    The 'Hosts' page has two main action buttons:
    'Add' and 'Delete'.
    
    The functionality has been implemented in the
    following components:
    - `AddHosts` will manage any 'Add hosts' operation.
    Only one single host can be added at a time.
    - `DeleteHosts` will manage any 'Delete hosts'
    operation. Those should be selected using the
    checkboxes located in the table or by using the
    Bulk selector.
    - `DeletedElementsTable` is a summary table that
    is shown in a confirmation modal to list the
    already selected elements to delete. This component
    has been implemented to be data-agnostic and reusable.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/components/modals/DeleteHosts.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+// PatternFly
+import {
+  TextContent,
+  Text,
+  TextVariants,
+  Button,
+} from "@patternfly/react-core";
+// Redux
+import { useAppDispatch, useAppSelector } from "src/store/hooks";
+import { removeHost } from "src/store/Identity/hosts-slice";
+// Layouts
+import ModalWithFormLayout from "../layouts/ModalWithFormLayout";
+// Tables
+import DeletedElementsTable from "src/components/tables/DeletedElementsTable";
+
+interface ButtonsData {
+  updateIsDeleteButtonDisabled: (value: boolean) => void;
+  updateIsDeletion: (value: boolean) => void;
+}
+
+interface SelectedHostsData {
+  selectedHosts: string[];
+  updateSelectedHosts: (newSelectedHosts: string[]) => void;
+}
+
+export interface PropsToDeleteHosts {
+  show: boolean;
+  handleModalToggle: () => void;
+  selectedHostsData: SelectedHostsData;
+  buttonsData: ButtonsData;
+}
+
+const DeleteHosts = (props: PropsToDeleteHosts) => {
+  // Set dispatch (Redux)
+  const dispatch = useAppDispatch();
+
+  // Retrieve all hosts list from Store and make a copy
+  const hostsList = useAppSelector((state) => state.hosts.hostsList);
+  const hostsListCopy = [...hostsList];
+
+  // Define the column names that will be displayed on the confirmation table.
+  // - NOTE: Camel-case should match with the property to show as it is defined in the data.
+  //    This variable will be coverted into word.
+  const deleteHostsColumnNames = ["hostName", "description"];
+
+  // List of fields
+  const fields = [
+    {
+      id: "question-text",
+      pfComponent: (
+        <TextContent>
+          <Text component={TextVariants.p}>
+            Are you sure you want to remove the selected entries from Hosts?
+          </Text>
+        </TextContent>
+      ),
+    },
+    {
+      id: "deleted-users-table",
+      pfComponent: (
+        <DeletedElementsTable
+          elementsToDelete={props.selectedHostsData.selectedHosts}
+          elementsList={hostsListCopy}
+          columnNames={deleteHostsColumnNames}
+          elementType="hosts"
+        />
+      ),
+    },
+  ];
+
+  // Close modal
+  const closeModal = () => {
+    props.handleModalToggle();
+  };
+
+  // Redux: Delete hosts
+  const deleteHosts = () => {
+    props.selectedHostsData.selectedHosts.map((host) => {
+      dispatch(removeHost(host));
+    });
+    props.selectedHostsData.updateSelectedHosts([]);
+    props.buttonsData.updateIsDeleteButtonDisabled(true);
+    props.buttonsData.updateIsDeletion(true);
+    closeModal();
+  };
+
+  // Set the Modal and Action buttons for 'Delete' option
+  const modalActionsDelete: JSX.Element[] = [
+    <Button
+      key="delete-hosts"
+      variant="danger"
+      onClick={deleteHosts}
+      form="delete-hosts-modal"
+    >
+      Delete
+    </Button>,
+    <Button key="cancel-delete-hosts" variant="link" onClick={closeModal}>
+      Cancel
+    </Button>,
+  ];
+
+  const modalDelete: JSX.Element = (
+    <ModalWithFormLayout
+      variantType="medium"
+      modalPosition="top"
+      offPosition="76px"
+      title="Remove hosts"
+      formId="remove-hosts-modal"
+      fields={fields}
+      show={props.show}
+      onClose={closeModal}
+      actions={modalActionsDelete}
+    />
+  );
+
+  // Render 'DeleteUsers'
+  return modalDelete;
+};
+
+export default DeleteHosts;
