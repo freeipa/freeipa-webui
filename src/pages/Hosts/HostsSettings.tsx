daebd45b124b34ecf0cb5b288fa841670f35524d
Carla Martinez <carlmart@redhat.com>
2023-01-24 16:39:39 +0100

    Create Host settings main section
    The 'Host' > 'Settings' section contains the
    main configuration for a specific host (accessed by
    clicking on the main table). This can be reachable
    under `<BASE-URL>/Hosts/Settings`.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/pages/Hosts/HostsSettings.tsx
@@ -0,0 +1,189 @@
+import React, { SyntheticEvent, useState } from "react";
+// PatternFly
+import {
+  DropdownDirection,
+  DropdownItem,
+  Flex,
+  JumpLinks,
+  JumpLinksItem,
+  PageSection,
+  PageSectionVariants,
+  Sidebar,
+  SidebarContent,
+  SidebarPanel,
+  TextVariants,
+} from "@patternfly/react-core";
+// Icons
+import OutlinedQuestionCircleIcon from "@patternfly/react-icons/dist/esm/icons/outlined-question-circle-icon";
+// Data types
+import { Host } from "src/utils/datatypes/globalDataTypes";
+// Layouts
+import HelpTextWithIconLayout from "src/components/layouts/HelpTextWithIconLayout";
+import TitleLayout from "src/components/layouts/TitleLayout";
+import SecondaryButton from "src/components/layouts/SecondaryButton";
+import KebabLayout from "src/components/layouts/KebabLayout";
+import ToolbarLayout from "src/components/layouts/ToolbarLayout";
+// Field sections
+import HostSettings from "src/components/HostsSections/HostSettings";
+import Enrollment from "src/components/HostsSections/Enrollment";
+import HostCertificate from "src/components/HostsSections/HostCertificate";
+import AllowedRetrieveKeytab from "src/components/HostsSections/AllowedRetrieveKeytab";
+import AllowedCreateKeytab from "src/components/HostsSections/AllowedCreateKeytab";
+
+interface PropsToHostsSettings {
+  host: Host;
+}
+
+const HostsSettings = (props: PropsToHostsSettings) => {
+  // Kebab
+  const [isKebabOpen, setIsKebabOpen] = useState(false);
+
+  const dropdownItems = [
+    <DropdownItem key="rebuild auto membership">
+      Rebuild auto membership
+    </DropdownItem>,
+    <DropdownItem key="unprovision">Unprovision</DropdownItem>,
+    <DropdownItem key="set-one-time-password">
+      Set one-time password
+    </DropdownItem>,
+    <DropdownItem key="new certificate">New certificate</DropdownItem>,
+  ];
+
+  const onKebabToggle = (isOpen: boolean) => {
+    setIsKebabOpen(isOpen);
+  };
+
+  const onKebabSelect = (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _event: SyntheticEvent<HTMLDivElement, Event> | undefined
+  ) => {
+    setIsKebabOpen(!isKebabOpen);
+  };
+  // Toolbar
+  const toolbarFields = [
+    {
+      key: 0,
+      element: <SecondaryButton>Refresh</SecondaryButton>,
+    },
+    {
+      key: 1,
+      element: <SecondaryButton isDisabled={true}>Revert</SecondaryButton>,
+    },
+    {
+      key: 2,
+      element: <SecondaryButton isDisabled={true}>Save</SecondaryButton>,
+    },
+    {
+      key: 3,
+      element: (
+        <KebabLayout
+          direction={DropdownDirection.up}
+          onDropdownSelect={onKebabSelect}
+          onKebabToggle={onKebabToggle}
+          idKebab="toggle-action-buttons"
+          isKebabOpen={isKebabOpen}
+          isPlain={true}
+          dropdownItems={dropdownItems}
+        />
+      ),
+    },
+  ];
+  return (
+    <>
+      <PageSection
+        id="settings-page"
+        variant={PageSectionVariants.light}
+        className="pf-u-pr-0 pf-u-ml-lg pf-u-mr-sm"
+        style={{ overflowY: "scroll", height: `calc(100vh - 319px)` }}
+      >
+        <Sidebar isPanelRight className="pf-u-mt-lg">
+          <SidebarPanel variant="sticky">
+            <HelpTextWithIconLayout
+              textComponent={TextVariants.p}
+              textClassName="pf-u-mb-md"
+              subTextComponent={TextVariants.a}
+              subTextIsVisitedLink={true}
+              textContent="Help"
+              icon={
+                <OutlinedQuestionCircleIcon className="pf-u-primary-color-100 pf-u-mr-sm" />
+              }
+            />
+            <JumpLinks
+              isVertical
+              label="Jump to section"
+              scrollableSelector="#settings-page"
+              offset={220} // for masthead
+              expandable={{ default: "expandable", md: "nonExpandable" }}
+            >
+              <JumpLinksItem key={0} href="#host-settings">
+                Host settings
+              </JumpLinksItem>
+              <JumpLinksItem key={1} href="#enrollment">
+                Enrollment
+              </JumpLinksItem>
+              <JumpLinksItem key={2} href="#host-certificate">
+                Host certificate
+              </JumpLinksItem>
+              <JumpLinksItem key={3} href="#allow-retrieve-keytab">
+                Allow to retrieve keytab
+              </JumpLinksItem>
+              <JumpLinksItem key={4} href="#allow-create-keytab">
+                Allow to create keytab
+              </JumpLinksItem>
+            </JumpLinks>
+          </SidebarPanel>
+
+          <SidebarContent className="pf-u-mr-xl">
+            <Flex
+              direction={{ default: "column" }}
+              flex={{ default: "flex_1" }}
+            >
+              <TitleLayout
+                key={0}
+                headingLevel="h2"
+                id="host-settings"
+                text="Host settings"
+              />
+              <HostSettings host={props.host} />
+              <TitleLayout
+                key={1}
+                headingLevel="h2"
+                id="enrollment"
+                text="Enrollment"
+              />
+              <Enrollment />
+              <TitleLayout
+                key={2}
+                headingLevel="h2"
+                id="host-certificate"
+                text="Host certificate"
+              />
+              <HostCertificate />
+              <TitleLayout
+                key={3}
+                headingLevel="h2"
+                id="allow-retrieve-keytab"
+                text="Allow to retrieve keytab"
+              />
+              <AllowedRetrieveKeytab host={props.host} />
+              <TitleLayout
+                key={4}
+                headingLevel="h2"
+                id="allow-create-keytab"
+                text="Allow to create keytab"
+              />
+              <AllowedCreateKeytab host={props.host} />
+            </Flex>
+          </SidebarContent>
+        </Sidebar>
+      </PageSection>
+      <ToolbarLayout
+        isSticky={true}
+        className={"pf-u-p-md pf-u-ml-lg pf-u-mr-lg"}
+        toolbarItems={toolbarFields}
+      />
+    </>
+  );
+};
+
+export default HostsSettings;
---
+++ src/pages/Hosts/HostsTabs.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+// PatternFly
+import {
+  Page,
+  PageSection,
+  PageSectionVariants,
+  Tabs,
+  Tab,
+  TabTitleText,
+} from "@patternfly/react-core";
+// React Router DOM
+import { useLocation } from "react-router-dom";
+// Navigation
+import { URL_PREFIX } from "src/navigation/NavRoutes";
+// Other
+import HostsSettings from "./HostsSettings";
+// Layouts
+import BreadcrumbLayout from "src/components/layouts/BreadcrumbLayout";
+import TitleLayout from "src/components/layouts/TitleLayout";
+// Data types
+import { Host } from "src/utils/datatypes/globalDataTypes";
+
+const HostsTabs = () => {
+  // TODO: Make the 'Tabs' component data-agnostic and more reusable,
+  //   getting the data via props.
+
+  // Get location (React Router DOM) and get state data
+  const location = useLocation();
+  const hostData: Host = location.state as Host;
+
+  // Tab
+  const [activeTabKey, setActiveTabKey] = useState(0);
+
+  const handleTabClick = (
+    _event: React.MouseEvent<HTMLElement, MouseEvent>,
+    tabIndex: number | string
+  ) => {
+    setActiveTabKey(tabIndex as number);
+  };
+
+  // 'pagesVisited' array will contain the visited pages.
+  // - Those will be passed to the 'BreadcrumbLayout' component.
+  const pagesVisited = [
+    {
+      name: "Hosts",
+      url: URL_PREFIX + "/hosts",
+    },
+  ];
+
+  return (
+    <Page>
+      <PageSection variant={PageSectionVariants.light} className="pf-u-pr-0">
+        <BreadcrumbLayout
+          className="pf-u-mb-md"
+          preText="Host:"
+          userId={hostData.id}
+          pagesVisited={pagesVisited}
+        />
+        <TitleLayout
+          id={hostData.hostName}
+          text={hostData.hostName}
+          headingLevel="h1"
+        />
+      </PageSection>
+      <PageSection type="tabs" variant={PageSectionVariants.light} isFilled>
+        <Tabs
+          activeKey={activeTabKey}
+          onSelect={handleTabClick}
+          variant="light300"
+          isBox
+          className="pf-u-ml-lg"
+        >
+          <Tab
+            eventKey={0}
+            name="details"
+            title={<TabTitleText>Settings</TabTitleText>}
+          >
+            <PageSection className="pf-u-pb-0"></PageSection>
+            <HostsSettings host={hostData} />
+          </Tab>
+        </Tabs>
+      </PageSection>
+    </Page>
+  );
+};
+
+export default HostsTabs;
