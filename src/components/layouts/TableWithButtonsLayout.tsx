4e809ae413a84797947590cb3a06e31b191051a6
Carla Martinez <carlmart@redhat.com>
2023-01-24 15:24:42 +0100

    Adapt and create layout components
    Some layout components need to be adapted to enhance
    its functionality (eg: `BreadCrumbLayout`).
    Others need to be created to be used in several parts of the
    code (eg: `TableWithButtonsLayout` and `TextLayout`).
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/components/layouts/TableWithButtonsLayout.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+// PatternFly
+import { Flex, FlexItem } from "@patternfly/react-core";
+import { TableVariant } from "@patternfly/react-table";
+// Layout
+import SecondaryButton from "./SecondaryButton";
+import TableLayout from "./TableLayout";
+
+export interface PropsToTableWithButtons {
+  // Table
+  ariaLabel: string;
+  name?: string;
+  variant: TableVariant | "compact";
+  hasBorders: boolean;
+  tableClasses?: string;
+  tableId: string;
+  isStickyHeader: boolean;
+  tableHeader?: JSX.Element;
+  tableBody: JSX.Element[] | JSX.Element;
+  // Buttons
+  deleteButtonClasses?: string;
+  onDeleteModal: () => void;
+  isDeleteDisabled?: boolean;
+  addButtonClasses?: string;
+  onAddModal: () => void;
+}
+
+const TableWithButtonsLayout = (props: PropsToTableWithButtons) => {
+  return (
+    <>
+      <Flex>
+        <FlexItem>
+          <SecondaryButton
+            classname={props.deleteButtonClasses}
+            isDisabled={props.isDeleteDisabled}
+            onClickHandler={props.onDeleteModal}
+          >
+            Delete
+          </SecondaryButton>
+          <SecondaryButton
+            classname={props.addButtonClasses}
+            onClickHandler={props.onAddModal}
+          >
+            Add
+          </SecondaryButton>
+        </FlexItem>
+      </Flex>
+      <TableLayout
+        ariaLabel={props.ariaLabel}
+        name={props.name}
+        variant={props.variant}
+        hasBorders={props.hasBorders}
+        classes={props.tableClasses}
+        tableId={props.tableId}
+        isStickyHeader={props.isStickyHeader}
+        tableHeader={props.tableHeader}
+        tableBody={props.tableBody}
+      />
+    </>
+  );
+};
+
+export default TableWithButtonsLayout;
---
+++ src/components/layouts/TextLayout.tsx
@@ -0,0 +1,49 @@
+// For further information, visit the PatternFly page: https://www.patternfly.org/v4/components/text
+import { TextContent, Text } from "@patternfly/react-core";
+import React from "react";
+
+interface PropsToTextLayout {
+  // 'TextContent' props
+  textContentClassName?: string;
+  textContentIsVisited?: boolean;
+  textContentChildren?: React.ReactNode;
+  // Text
+  className?: string;
+  component?:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "p"
+    | "a"
+    | "small"
+    | "blockquote"
+    | "pre";
+  isvisitedLink?: boolean;
+  ouiaId?: string | number;
+  ouiaSafe?: boolean;
+  children: React.ReactNode;
+}
+
+const TextLayout = (props: PropsToTextLayout) => {
+  return (
+    <TextContent
+      className={props.textContentClassName}
+      isVisited={props.textContentIsVisited}
+    >
+      {props.textContentChildren}
+      <Text
+        component={props.component}
+        isVisitedLink={props.isvisitedLink}
+        ouiaId={props.ouiaId}
+        ouiaSafe={props.ouiaSafe}
+      >
+        {props.children}
+      </Text>
+    </TextContent>
+  );
+};
+
+export default TextLayout;
