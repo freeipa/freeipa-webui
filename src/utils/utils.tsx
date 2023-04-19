a0f60cf95ce0d3ab84f94f29e0510a755ccdf2bf
Carla Martinez <carlmart@redhat.com>
2023-04-19 11:03:23 +0200

    Retrieve global and user info via API calls
    Once the RPC wrapper is defined and the data types
    adapted, the entrypoints can be called from the
    components to retrieve data from the IPA API.
    
    So on the one hand, the `ActiveUsers` component
    should retrieve the user data to be shown in the
    table (as well as handling possible errors).
    On the other hand, the initial calls can be done
    from the `App` component directly, as this one can
    conditionally render the rest of the components when
    the initial API call has been completed.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/utils/utils.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+// Data type
+import { Host, Service, User } from "./datatypes/globalDataTypes";
+// Errors
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
+import { SerializedError } from "@reduxjs/toolkit";
+// PatternFly
+import TextLayout from "src/components/layouts/TextLayout";
+
+/*
+ * Functions that can be reusable and called by several components throughout the application.
+ */
+
+// Default API version (in case this is not available in Redux ATM)
+export const API_VERSION_BACKUP = "2.251";
+
+// Helper method: Given an users list and status, check if some entry has different status
+export const checkEqualStatus = (status: boolean, usersList: User[]) => {
+  const usersWithOtherStatus = usersList.filter(
+    (user) => user.nsaccountlock !== status
+  );
+  return usersWithOtherStatus.length === 0;
+};
+
+// Determine whether a user is selectable or not
+export const isUserSelectable = (user: User) => user.uid !== "";
+
+// Determine whether a host is selectable or not
+export const isHostSelectable = (host: Host) => host.id != "";
+
+// Determine whether a service is selectable or not
+export const isServiceSelectable = (service: Service) => service.id != "";
+
+// Write JSX error messages into 'apiErrorsJsx' array
+export const apiErrorToJsXError = (
+  errorFromApiCall: FetchBaseQueryError | SerializedError,
+  contextMessage: string,
+  key: string
+) => {
+  let errorJsx: JSX.Element = <></>;
+
+  if ("originalStatus" in errorFromApiCall) {
+    // The original status is accessible here (error 401)
+    errorJsx = (
+      <TextLayout component="p" key={key}>
+        {errorFromApiCall.originalStatus + " " + contextMessage}
+      </TextLayout>
+    );
+  } else if ("status" in errorFromApiCall) {
+    // you can access all properties of `FetchBaseQueryError` here
+    errorJsx = (
+      <TextLayout component="p" key={key}>
+        {errorFromApiCall.status + " " + contextMessage}
+      </TextLayout>
+    );
+  } else {
+    // you can access all properties of `SerializedError` here
+    errorJsx = (
+      <div key={key} style={{ alignSelf: "center", marginTop: "16px" }}>
+        <TextLayout component="p">{contextMessage}</TextLayout>
+        <TextLayout component="p">
+          {"ERROR CODE: " + errorFromApiCall.code}
+        </TextLayout>
+        <TextLayout component="p">{errorFromApiCall.message}</TextLayout>
+      </div>
+    );
+  }
+
+  return errorJsx;
+};
