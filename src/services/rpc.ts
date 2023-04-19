bb6f30fbdbe6c9e1ea4db13a438694e3955775b1
Carla Martinez <carlmart@redhat.com>
2023-04-19 09:49:41 +0200

    Create RPC wrapper to make API calls
    The main goal of the communication layer is to
    perform API calls to retrieve and mutate data
    from the IPA server via JSON-RPC. The RTK Query
    library[1] an optional addon included in the
    Redux Toolkit package and it is designed to
    simplify common cases for loading data in a
    web application. Also, it is very versatile and
    can be adapted to any type of API protocol.
    
    As a first step, the file to manage the different
    API calls needs to be created. Later on, those
    entrypoints could be consumed by other components
    in order to perform the API calls. Considering that
    the entrypoints can be defined as Queries[2] or
    Mutations[3] basic types, the solution should
    use that logic to define a set of endpoints that
    could manage three types of operations:
    - Individual calls
    - Multiple calls (batch)
    - Sequential calls (when two different calls are
      executed in a single endpoint to perform an operation
      and the second call could not start until the first
      one has finished).
    
    [1]- https://redux-toolkit.js.org/rtk-query/overview
    [2]- https://redux-toolkit.js.org/rtk-query/usage/queries
    [3]- https://redux-toolkit.js.org/rtk-query/usage/mutations
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/services/rpc.ts
@@ -0,0 +1,197 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// RTK Query
+import { SerializedError } from "@reduxjs/toolkit";
+import {
+  createApi,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
+// Utils
+import { API_VERSION_BACKUP } from "src/utils/utils";
+
+export interface UIDType {
+  dn: string;
+  uid: string[];
+}
+
+export interface Query {
+  data: FindRPCResponse | BatchRPCResponse | undefined;
+  isLoading: boolean;
+  error: FetchBaseQueryError | SerializedError | undefined;
+}
+
+// 'FindRPCResponse' type
+//   - Has 'result' > 'result' structure
+export interface FindRPCResponse {
+  error: string;
+  id: string;
+  principal: string;
+  version: string;
+  result: {
+    result: Record<string, unknown>; // General object type
+    count: number;
+    truncated: boolean;
+    summary: string;
+  };
+}
+
+// 'BatchRPCResponse' type
+//   - Has 'result' > 'results' > 'result' structure
+//   - More data under 'result'
+export interface BatchRPCResponse {
+  error: string;
+  id: string;
+  principal: string;
+  version: string;
+  result: {
+    count: number;
+    results: {
+      result: Record<string, unknown>; // General object type
+      truncated: boolean;
+      summary: string;
+    };
+  };
+}
+
+export interface CommandWithSingleParam {
+  command: string;
+  param: string;
+}
+
+export interface Command {
+  method: string;
+  params: any[];
+}
+
+export interface BatchCommand {
+  batch: CommandWithSingleParam[];
+}
+
+// Body data to perform the calls
+export const getCommand = (commandData: Command) => {
+  const payloadWithParams = {
+    url: "ipa/session/json",
+    method: "POST",
+    body: {
+      method: commandData.method,
+      params: commandData.params,
+    },
+  };
+  return payloadWithParams;
+};
+
+export const getBatchCommand = (commandData: Command[], apiVersion: string) => {
+  const payloadBatchParams = {
+    url: "ipa/session/json",
+    method: "POST",
+    body: {
+      method: "batch",
+      params: [
+        commandData,
+        {
+          version: apiVersion,
+        },
+      ],
+    },
+  };
+  return payloadBatchParams;
+};
+
+// Endpoints that will be called from anywhere in the application.
+// Two types:
+//   - Queries: https://redux-toolkit.js.org/rtk-query/usage/queries
+//   - Mutations: https://redux-toolkit.js.org/rtk-query/usage/mutations
+// Endpoints can perform individual calls (e.g: `simpleCommand`), multiple commands
+//   (e.g: `batchCommand`), and multiple commands executed sequentially in a single
+//   endpoint (e.g: `gettingUserData`)
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }), // TODO: Global settings!
+  endpoints: (build) => ({
+    simpleCommand: build.query<FindRPCResponse, Command | void>({
+      query: (payloadData: Command) => getCommand(payloadData),
+    }),
+    simpleMutCommand: build.mutation<BatchRPCResponse, Command>({
+      query: (payloadData: Command) => getCommand(payloadData),
+    }),
+    batchCommand: build.query<BatchRPCResponse, Command[] | void>({
+      query: (payloadData: Command[], apiVersion?: string) =>
+        getBatchCommand(payloadData, apiVersion || API_VERSION_BACKUP),
+    }),
+    batchMutCommand: build.mutation<BatchRPCResponse, Command[] | void>({
+      query: (payloadData: Command[], apiVersion?: string) =>
+        getBatchCommand(payloadData, apiVersion || API_VERSION_BACKUP),
+    }),
+    gettingUserData: build.query<BatchRPCResponse, string | void>({
+      async queryFn(apiVersion, _queryApi, _extraOptions, fetchWithBQ) {
+        // 1ST CALL - GETTING ALL UIDS
+        if (apiVersion === undefined) {
+          return {
+            error: {
+              status: "CUSTOM_ERROR",
+              data: "",
+              error: "API version not available",
+            } as FetchBaseQueryError,
+          };
+        }
+        // Prepare payload
+        const payloadDataUids: Command = {
+          method: "user_find",
+          params: [
+            [],
+            {
+              pkey_only: true,
+              sizelimit: 0,
+              version: apiVersion,
+            },
+          ],
+        };
+
+        // Make call using 'fetchWithBQ'
+        const getUidsResult = await fetchWithBQ(getCommand(payloadDataUids));
+        // Return possible errors
+        if (getUidsResult.error) {
+          return { error: getUidsResult.error as FetchBaseQueryError };
+        }
+        // If no error: cast and assign 'uids'
+        const uidResponseData = getUidsResult.data as FindRPCResponse;
+
+        const uids: string[] = [];
+        const itemsCount = uidResponseData.result.result.length as number;
+        for (let i = 0; i < itemsCount; i++) {
+          const userId = uidResponseData.result.result[i] as UIDType;
+          const { uid } = userId;
+          uids.push(uid[0] as string);
+        }
+
+        // 2ND CALL - GET PARTIAL USERS INFO
+        // Prepare payload
+        const options = { no_members: true };
+        const payloadUserDataBatch: Command[] = uids.map((uid) => ({
+          method: "user_show",
+          params: [[uid], options],
+        }));
+
+        // Make call using 'fetchWithBQ'
+        const partialUsersInfoResult = await fetchWithBQ(
+          getBatchCommand(payloadUserDataBatch as Command[], apiVersion)
+        );
+
+        // Return results
+        return partialUsersInfoResult.data
+          ? { data: partialUsersInfoResult.data as BatchRPCResponse }
+          : {
+              error:
+                partialUsersInfoResult.error as unknown as FetchBaseQueryError,
+            };
+      },
+    }),
+  }),
+});
+
+export const {
+  useSimpleCommandQuery,
+  useSimpleMutCommandMutation,
+  useBatchCommandQuery,
+  useBatchMutCommandMutation,
+  useGettingUserDataQuery,
+} = api;
