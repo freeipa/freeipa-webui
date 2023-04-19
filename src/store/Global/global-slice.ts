0b6583489c248e349298312574b7f53c9db7ada3
Carla Martinez <carlmart@redhat.com>
2023-04-19 10:20:25 +0200

    Define global slice
    According to the current WebUI, one of the first
    API calls that are made when the user logs-in is
    to retrieve environmental parameters, information
    of the logged in user, and other useful data that
    is needed to set up the WebUI. Ideally, this data
    should be accessible by any component of the
    application in order to perform further operations
    (e.g: the API version is needed to perform API calls).
    
    In order to store this information, a new Redux
    slice (`global-slice.ts`) should be created and
    added into the global store (`store`).
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/store/Global/global-slice.ts
@@ -0,0 +1,99 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface GlobalState {
+  // TODO: Specify data types
+  ipaServerConfiguration: Record<string, unknown>;
+  loggedUserInfo: Record<string, unknown>;
+  environment: Record<string, unknown>;
+  dnsIsEnabled: Record<string, unknown>;
+  trustConfiguration: Record<string, unknown>;
+  domainLevel: Record<string, unknown>;
+  caIsEnabled: Record<string, unknown>;
+  vaultConfiguration: Record<string, unknown>;
+}
+
+const initialState: GlobalState = {
+  ipaServerConfiguration: {},
+  loggedUserInfo: {},
+  environment: {},
+  dnsIsEnabled: {},
+  trustConfiguration: {},
+  domainLevel: {},
+  caIsEnabled: {},
+  vaultConfiguration: {},
+};
+
+const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    updateIpaServerConfiguration: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newIpaServerConfig = action.payload;
+      state.ipaServerConfiguration = newIpaServerConfig;
+    },
+    updateLoggedUserInfo: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newLoggedUserInfo = action.payload;
+      state.loggedUserInfo = newLoggedUserInfo;
+    },
+    updateEnvironment: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newEnv = action.payload;
+      state.environment = newEnv;
+    },
+    updateDnsIsEnabled: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newDnsIsEnabled = action.payload;
+      state.dnsIsEnabled = newDnsIsEnabled;
+    },
+    updateTrustConfiguration: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newTrustConfig = action.payload;
+      state.trustConfiguration = newTrustConfig;
+    },
+    updateDomainLevel: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newDomainLevel = action.payload;
+      state.domainLevel = newDomainLevel;
+    },
+    updateCaIsEnabled: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newCaIsEnabled = action.payload;
+      state.caIsEnabled = newCaIsEnabled;
+    },
+    updateVaultConfiguration: (
+      state,
+      action: PayloadAction<Record<string, unknown>>
+    ) => {
+      const newVaultConfig = action.payload;
+      state.vaultConfiguration = newVaultConfig;
+    },
+  },
+});
+
+export const {
+  updateIpaServerConfiguration,
+  updateLoggedUserInfo,
+  updateEnvironment,
+  updateDnsIsEnabled,
+  updateTrustConfiguration,
+  updateDomainLevel,
+  updateCaIsEnabled,
+  updateVaultConfiguration,
+} = globalSlice.actions;
+export default globalSlice.reducer;
