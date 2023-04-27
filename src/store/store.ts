34e0fa9e996938686d4f464e8a7c82777d6f54c4
Carla Martinez <carlmart@redhat.com>
2023-04-27 10:02:54 +0200

    Configure the store for the api wrapper
    According to [1], the created API slice (our wrapper)
    can be set as a Redux slice to track which calls
    have been made (and its responses) on which endpoint.
    
    [1]- https://redux-toolkit.js.org/rtk-query/overview#configure-the-store
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import activeUsersReducer from "./Identity/activeUsers-slice";
 import netgroupsReducer from "./Identity/netgroups-slice";
 import userGroupsReducer from "./Identity/userGroups-slice";
@@ -10,9 +11,11 @@ import preservedUsersReducer from "./Identity/preservedUsers-slice";
 import hostsReducer from "./Identity/hosts-slice";
 import hostGroupsReducer from "./Identity/hostGroups-slice";
 import servicesReducer from "./Identity/services-slice";
+import { api } from "../services/rpc";
 
 const store = configureStore({
   reducer: {
+    api: api.reducer,
     activeUsers: activeUsersReducer,
     netgroups: netgroupsReducer,
     usergroups: userGroupsReducer,
@@ -25,7 +28,15 @@ const store = configureStore({
     hostGroups: hostGroupsReducer,
     services: servicesReducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
