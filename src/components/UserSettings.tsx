bf9145d1cc1c46f6d68d9935555ad5c26e5b402a
Carla Martinez <carlmart@redhat.com>
2023-05-30 16:42:01 +0200

    Retrieve API data and sync user fields
    The fields of the 'Active users' > 'Settings' section
    need to show the IPA server data values.
    
    The following calls are made when the user 'Settings'
    page is loaded:
    - Batch
      - `user_show` (with `{all: true, rights: true}` params)
        retrieves the full user data.
      - `pwpolicy_show` retrieves the password policy data.
      - `krbtpolicy_show` retrieves the kerberos ticket
        policy data.
      - `cert_find` retrieves the list of certificates.
    - `radiusproxy_find` retrieves the list of RADIUS servers.
    - `idp_find` retrieves the list of IdP servers.
    - `user_find` retrieves the list of user IDs.
    
    In order to manage the data coming from the certificates
    and RADIUS servers, a new data types have been defined in
    the `globalDataTypes.ts` file: `Certificate` and `RadiusServer`.
    Some components have been adapted to take those types (e.g:
    `CertificateMappingDataModal`)
    
    The data from the IPA server has been retrieved from the
    `UserSettings` component and has been propagated to the
    following subsection components via props:
    - `UsersIdentity`: Identity settings
    - `UsersAccountSettings`: Account settings
    - `UsersPasswordPolicy`: Password policy
    - `UsersKerberosTicket`: Kerberos ticket policy
    - `UsersContactSettings`: Contact settings
    - `UsersMailingAddress`: Mailing address
    - `UsersEmployeeInfo`: Employee information
    - `UsersAttributesSMB`: User attributes for SMB services
    
    The interaction between the API call response status and
    the UI has been managed using a Spinner[1] component.
    
    [1]- https://www.patternfly.org/v4/components/spinner/react
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -1,4 +1,5 @@
-import React, { SyntheticEvent, useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { SyntheticEvent, useEffect, useState } from "react";
 // PatternFly
 import {
   PageSection,
@@ -12,6 +13,7 @@ import {
   SidebarPanel,
   SidebarContent,
   DropdownDirection,
+  Spinner,
 } from "@patternfly/react-core";
 // Icons
 import OutlinedQuestionCircleIcon from "@patternfly/react-icons/dist/esm/icons/outlined-question-circle-icon";
@@ -32,6 +34,14 @@ import UsersContactSettings from "src/components/UsersSections/UsersContactSetti
 import UsersMailingAddress from "src/components/UsersSections/UsersMailingAddress";
 import UsersEmployeeInfo from "src/components/UsersSections/UsersEmployeeInfo";
 import UsersAttributesSMB from "src/components/UsersSections/UsersAttributesSMB";
+// Redux
+import { useAppSelector } from "src/store/hooks";
+// RPC
+import {
+  Command,
+  useBatchCommandQuery,
+  useSimpleCommandQuery,
+} from "src/services/rpc";
 
 export interface PropsToUserSettings {
   user: User;
@@ -39,6 +49,114 @@ export interface PropsToUserSettings {
 }
 
 const UserSettings = (props: PropsToUserSettings) => {
+  // Retrieve API version from environment data
+  const apiVersion = useAppSelector(
+    (state) => state.global.environment.api_version
+  ) as string;
+
+  // Initialize data
+  const [userShowData, setUserShowData] = useState<Record<string, unknown>>();
+  const [pwpolicyShowData, setPwpolicyShowData] =
+    useState<Record<string, unknown>>();
+  const [krbtpolicyShowData, setKrbtpolicyShowData] =
+    useState<Record<string, unknown>>();
+  const [certFindData, setCertFindData] = useState<Record<string, unknown>>();
+  const [radiusProxyData, setRadiusProxyData] =
+    useState<Record<string, unknown>>();
+  const [idpData, setIdpData] = useState<Record<string, unknown>>();
+  const [uidsData, setUidsData] = useState<Record<string, unknown>>();
+
+  // [API call] Get full user information
+  const userShowCommand: Command = {
+    method: "user_show",
+    params: [props.user.uid, { all: true, rights: true }],
+  };
+
+  const pwpolicyShowCommand: Command = {
+    method: "pwpolicy_show",
+    params: [[], { user: props.user.uid[0], all: true, rights: true }],
+  };
+
+  const krbtpolicyShowCommand: Command = {
+    method: "krbtpolicy_show",
+    params: [props.user.uid, { all: true, rights: true }],
+  };
+
+  const certFindCommand: Command = {
+    method: "cert_find",
+    params: [[], { user: props.user.uid[0], sizelimit: 0, all: true }],
+  };
+
+  const batchPayload: Command[] = [
+    userShowCommand,
+    pwpolicyShowCommand,
+    krbtpolicyShowCommand,
+    certFindCommand,
+  ];
+
+  const { data: batchResponse, isLoading: isBatchLoading } =
+    useBatchCommandQuery(batchPayload);
+
+  // [API calls] Get context information
+  // - RADIUS Proxy
+  const radiusproxyPayload: Command = {
+    method: "radiusproxy_find",
+    params: [[], { version: apiVersion }],
+  };
+
+  const { data: radiusProxyResponse, isLoading: isRadiusProxyLoading } =
+    useSimpleCommandQuery(radiusproxyPayload);
+
+  // - External IPD
+  const idpPayload: Command = {
+    method: "idp_find",
+    params: [[], { version: apiVersion }],
+  };
+
+  const { data: idpResponse, isLoading: isIdpLoading } =
+    useSimpleCommandQuery(idpPayload);
+
+  // - User IDs
+  const userIdsPayload: Command = {
+    method: "user_find",
+    params: [[], { no_members: true, version: apiVersion }],
+  };
+
+  const { data: uidsResponse, isLoading: isUidsLoading } =
+    useSimpleCommandQuery(userIdsPayload);
+
+  // Update batch data when available
+  useEffect(() => {
+    // Batch
+    if (batchResponse !== undefined) {
+      setUserShowData(batchResponse.result.results[0].result);
+      setPwpolicyShowData(batchResponse.result.results[1].result);
+      setKrbtpolicyShowData(batchResponse.result.results[2].result);
+      setCertFindData(batchResponse.result.results[3].result);
+    }
+  }, [isBatchLoading]);
+
+  // Update RADIUS proxy data when available
+  useEffect(() => {
+    if (radiusProxyResponse !== undefined) {
+      setRadiusProxyData(radiusProxyResponse.result.result);
+    }
+  }, [isRadiusProxyLoading]);
+
+  // Update External IdP data when available
+  useEffect(() => {
+    if (idpResponse !== undefined) {
+      setIdpData(idpResponse.result.result);
+    }
+  }, [isIdpLoading]);
+
+  // Update User IDs data when available
+  useEffect(() => {
+    if (uidsResponse !== undefined) {
+      setUidsData(uidsResponse.result.result);
+    }
+  }, [isUidsLoading]);
+
   // Kebab
   const [isKebabOpen, setIsKebabOpen] = useState(false);
 
@@ -69,6 +187,15 @@ const UserSettings = (props: PropsToUserSettings) => {
   ) => {
     setIsKebabOpen(!isKebabOpen);
   };
+
+  // Spinner UI
+  const spinner = (
+    <Spinner
+      isSVG
+      style={{ alignSelf: "center", marginLeft: "45%", marginTop: "5%" }}
+      aria-label="Loading contents of user settings"
+    />
+  );
 
   // Toolbar
   const toolbarFields = [
@@ -109,116 +236,131 @@ const UserSettings = (props: PropsToUserSettings) => {
         className="pf-u-pr-0 pf-u-ml-lg pf-u-mr-sm"
         style={{ overflowY: "scroll", height: `calc(100vh - 319.2px)` }}
       >
-        <Sidebar isPanelRight className="pf-u-mt-lg">
-          <SidebarPanel variant="sticky">
-            <HelpTextWithIconLayout
-              textComponent={TextVariants.p}
-              textClassName="pf-u-mb-md"
-              subTextComponent={TextVariants.a}
-              subTextIsVisitedLink={true}
-              textContent="Help"
-              icon={
-                <OutlinedQuestionCircleIcon className="pf-u-primary-color-100 pf-u-mr-sm" />
-              }
-            />
-            <JumpLinks
-              isVertical
-              label="Jump to section"
-              scrollableSelector="#settings-page"
-              offset={220} // for masthead
-              expandable={{ default: "expandable", md: "nonExpandable" }}
-            >
-              <JumpLinksItem key={0} href="#identity-settings">
-                Identity settings
-              </JumpLinksItem>
-              <JumpLinksItem key={1} href="#account-settings">
-                Account settings
-              </JumpLinksItem>
-              <JumpLinksItem key={2} href="#password-policy">
-                Password policy
-              </JumpLinksItem>
-              <JumpLinksItem key={3} href="#kerberos-ticket">
-                Kerberos ticket policy
-              </JumpLinksItem>
-              <JumpLinksItem key={4} href="#contact-settings">
-                Contact settings
-              </JumpLinksItem>
-              <JumpLinksItem key={5} href="#mailing-address">
-                Mailing address
-              </JumpLinksItem>
-              <JumpLinksItem key={6} href="#employee-information">
-                Employee information
-              </JumpLinksItem>
-              <JumpLinksItem key={7} href="#smb-services">
-                User attributes for SMB services
-              </JumpLinksItem>
-            </JumpLinks>
-          </SidebarPanel>
+        {!isBatchLoading &&
+        !isRadiusProxyLoading &&
+        !isIdpLoading &&
+        !isUidsLoading ? (
+          <Sidebar isPanelRight className="pf-u-mt-lg">
+            <SidebarPanel variant="sticky">
+              <HelpTextWithIconLayout
+                textComponent={TextVariants.p}
+                textClassName="pf-u-mb-md"
+                subTextComponent={TextVariants.a}
+                subTextIsVisitedLink={true}
+                textContent="Help"
+                icon={
+                  <OutlinedQuestionCircleIcon className="pf-u-primary-color-100 pf-u-mr-sm" />
+                }
+              />
+              <JumpLinks
+                isVertical
+                label="Jump to section"
+                scrollableSelector="#settings-page"
+                offset={220} // for masthead
+                expandable={{ default: "expandable", md: "nonExpandable" }}
+              >
+                <JumpLinksItem key={0} href="#identity-settings">
+                  Identity settings
+                </JumpLinksItem>
+                <JumpLinksItem key={1} href="#account-settings">
+                  Account settings
+                </JumpLinksItem>
+                <JumpLinksItem key={2} href="#password-policy">
+                  Password policy
+                </JumpLinksItem>
+                <JumpLinksItem key={3} href="#kerberos-ticket">
+                  Kerberos ticket policy
+                </JumpLinksItem>
+                <JumpLinksItem key={4} href="#contact-settings">
+                  Contact settings
+                </JumpLinksItem>
+                <JumpLinksItem key={5} href="#mailing-address">
+                  Mailing address
+                </JumpLinksItem>
+                <JumpLinksItem key={6} href="#employee-information">
+                  Employee information
+                </JumpLinksItem>
+                <JumpLinksItem key={7} href="#smb-services">
+                  User attributes for SMB services
+                </JumpLinksItem>
+              </JumpLinks>
+            </SidebarPanel>
 
-          <SidebarContent className="pf-u-mr-xl">
-            <Flex
-              direction={{ default: "column" }}
-              flex={{ default: "flex_1" }}
-            >
-              <TitleLayout
-                key={0}
-                headingLevel="h2"
-                id="identity-settings"
-                text="Identity settings"
-              />
-              <UsersIdentity user={props.user} />
-              <TitleLayout
-                key={1}
-                headingLevel="h2"
-                id="account-settings"
-                text="Account settings"
-              />
-              <UsersAccountSettings user={props.user} />
-              <TitleLayout
-                key={2}
-                headingLevel="h2"
-                id="password-policy"
-                text="Password policy"
-              />
-              <UsersPasswordPolicy />
-              <TitleLayout
-                key={3}
-                headingLevel="h2"
-                id="kerberos-ticket"
-                text="Kerberos ticket"
-              />
-              <UsersKerberosTicket />
-              <TitleLayout
-                key={4}
-                headingLevel="h2"
-                id="contact-settings"
-                text="Contact settings"
-              />
-              <UsersContactSettings user={props.user} />
-              <TitleLayout
-                key={5}
-                headingLevel="h2"
-                id="mailing-address"
-                text="Mailing address"
-              />
-              <UsersMailingAddress />
-              <TitleLayout
-                key={6}
-                headingLevel="h2"
-                id="employee-information"
-                text="Employee information"
-              />
-              <UsersEmployeeInfo />
-              <TitleLayout
-                key={7}
-                headingLevel="h2"
-                id="smb-services"
-                text="User attributes for SMB services"
-              />
-              <UsersAttributesSMB />
-            </Flex>
-          </SidebarContent>
-        </Sidebar>
+            <SidebarContent className="pf-u-mr-xl">
+              <Flex
+                direction={{ default: "column" }}
+                flex={{ default: "flex_1" }}
+              >
+                <TitleLayout
+                  key={0}
+                  headingLevel="h2"
+                  id="identity-settings"
+                  text="Identity settings"
+                />
+                <UsersIdentity userData={userShowData} />
+                <TitleLayout
+                  key={1}
+                  headingLevel="h2"
+                  id="account-settings"
+                  text="Account settings"
+                />
+                <UsersAccountSettings
+                  userData={userShowData}
+                  certData={certFindData}
+                  radiusProxyData={radiusProxyData}
+                  idpData={idpData}
+                />
+                <TitleLayout
+                  key={2}
+                  headingLevel="h2"
+                  id="password-policy"
+                  text="Password policy"
+                />
+                <UsersPasswordPolicy pwpolicyData={pwpolicyShowData} />
+                <TitleLayout
+                  key={3}
+                  headingLevel="h2"
+                  id="kerberos-ticket"
+                  text="Kerberos ticket"
+                />
+                <UsersKerberosTicket krbtpolicyData={krbtpolicyShowData} />
+                <TitleLayout
+                  key={4}
+                  headingLevel="h2"
+                  id="contact-settings"
+                  text="Contact settings"
+                />
+                <UsersContactSettings userData={userShowData} />
+                <TitleLayout
+                  key={5}
+                  headingLevel="h2"
+                  id="mailing-address"
+                  text="Mailing address"
+                />
+                <UsersMailingAddress userData={userShowData} />
+                <TitleLayout
+                  key={6}
+                  headingLevel="h2"
+                  id="employee-information"
+                  text="Employee information"
+                />
+                <UsersEmployeeInfo
+                  uidsData={uidsData}
+                  userData={userShowData}
+                />
+                <TitleLayout
+                  key={7}
+                  headingLevel="h2"
+                  id="smb-services"
+                  text="User attributes for SMB services"
+                />
+                <UsersAttributesSMB userData={userShowData} />
+              </Flex>
+            </SidebarContent>
+          </Sidebar>
+        ) : (
+          spinner
+        )}
       </PageSection>
       <ToolbarLayout
         isSticky={true}
