3602859a07264424b54dd4e20ab50b4cd2b1f0b1
Carla Martinez <carlmart@redhat.com>
2023-09-04 13:53:24 +0200

    Add functionality to Date time selector
    The Date-time picker[1] component needs to be adapted
    to take the data from the metadata and keep managing
    its functionality while using other components/wrappers.
    
    This is applied to the following field: 'Kerberos principal
    expiration (UTC).
    
    [1] - http://v4-archive.patternfly.org/v4/components/date-picker
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -84,3 +84,148 @@ export const getRealmFromKrbPolicy = (metadata: Metadata) => {
   }
   return realm;
 };
+
+// Date time parameters
+const templates = {
+  human: "YYYY-MM-DD HH:mm:ssZ",
+  generalized: "YYYYMMDDHHmmssZ",
+};
+
+const dates = [
+  ["YYYY-MM-DD", /^(\d{4})-(\d{2})-(\d{2})$/],
+  ["YYYYMMDD", /^(\d{4})(\d{2})(\d{2})$/],
+];
+
+const times = [
+  ["HH:mm:ss", /^(\d\d):(\d\d):(\d\d)$/],
+  ["HH:mm", /^(\d\d):(\d\d)$/],
+];
+
+const generalized_regex = /^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})Z$/;
+const datetime_regex =
+  /^((?:\d{8})|(?:\d{4}-\d{2}-\d{2}))(?:(T| )(\d\d:\d\d(?::\d\d)?)(Z?))?$/;
+
+// Parse full string date to UTC date format
+// '20230809120244Z' --> 'Sat Aug 09 2023 14:02:44 GMT+0200 (Central European Summer Time)'
+export const parseFullDateStringToUTCFormat = (dateString: string) => {
+  let Y = 0,
+    M = 0,
+    D = 0,
+    H = 0,
+    m = 0,
+    s = 0;
+  let i, l, dateStr, timeStr, utc;
+
+  const dt_match = datetime_regex.exec(dateString);
+  const gt_match = generalized_regex.exec(dateString);
+
+  if (dt_match) {
+    dateStr = dt_match[1];
+    timeStr = dt_match[3];
+    utc = dt_match[4] || !timeStr;
+
+    // error out if local time not supported
+    if (!utc) return null;
+
+    for (i = 0, l = dates.length; i < l; i++) {
+      const dm = dates[i][1] as RegExp;
+      dm.exec(dateStr);
+
+      if (dm) {
+        Y = dm[1];
+        M = dm[2];
+        D = dm[3];
+        break;
+      }
+    }
+
+    if (timeStr) {
+      for (i = 0, l = times.length; i < l; i++) {
+        const tm = times[i][1] as RegExp;
+        tm.exec(timeStr);
+        if (tm) {
+          H = tm[1];
+          m = tm[2] || 0;
+          s = tm[3] || 0;
+          break;
+        }
+      }
+    }
+  } else if (gt_match) {
+    Y = +gt_match[1];
+    M = +gt_match[2];
+    D = +gt_match[3];
+    H = +gt_match[4];
+    m = +gt_match[5];
+    s = +gt_match[6];
+    utc = true;
+  } else {
+    return null;
+  }
+
+  const date = new Date();
+
+  if (utc || !timeStr) {
+    date.setUTCFullYear(Y, M - 1, D);
+    date.setUTCHours(H, m, s, 0);
+  } else {
+    date.setFullYear(Y, M - 1, D);
+    date.setHours(H, m, s, 0);
+  }
+
+  return date;
+};
+
+// Given a date, obtain the time in LDAP generalized time format
+const formatDate = (date, format, local) => {
+  const fmt = format || templates.human;
+  let str;
+
+  if (local) {
+    const year = fmt.replace(/YYYY/i, date.getFullYear());
+    const month = year.replace(
+      /MM/i,
+      (date.getMonth() + 1).toString().padStart(2, "0")
+    );
+    const day = month.replace(
+      /DD/i,
+      date.getDate().toString().padStart(2, "0")
+    );
+    const hour = day.replace(
+      /HH/i,
+      date.getHours().toString().padStart(2, "0")
+    );
+    const minute = hour.replace(
+      /mm/i,
+      date.getMinutes().toString().padStart(2, "0")
+    );
+    str = minute.replace(/ss/i, date.getSeconds().toString().padStart(2, "0"));
+  } else {
+    const year = fmt.replace(/YYYY/i, date.getUTCFullYear());
+    const month = year.replace(
+      /MM/i,
+      (date.getUTCMonth() + 1).toString().padStart(2, "0")
+    );
+    const day = month.replace(
+      /DD/i,
+      date.getUTCDate().toString().padStart(2, "0")
+    );
+    const hour = day.replace(
+      /HH/i,
+      date.getUTCHours().toString().padStart(2, "0")
+    );
+    const minute = hour.replace(
+      /mm/i,
+      date.getUTCMinutes().toString().padStart(2, "0")
+    );
+    str = minute.replace(
+      /ss/i,
+      date.getUTCSeconds().toString().padStart(2, "0")
+    );
+  }
+  return str;
+};
+
+export const toGeneralizedTime = (date: Date) => {
+  return formatDate(date, templates.generalized, false);
+};
