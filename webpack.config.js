ceef882d432dd9569070f913d695b4f9817e5317
heben <hb1230123@hotmail.com>
2023-09-07 10:53:17 +0800

    feat: add webpack proxy config
    Add historyApiFallback and publicPath for browser refresh.
    Add proxy config to connect server.

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,12 @@ module.exports = {
     app: ["./src/index.tsx"],
     vendor: ["react", "react-dom"],
   },
-
   output: {
     path: path.resolve(__dirname, "dist"),
     clean: true,
     filename: "js/[name].bundle.js",
+    publicPath: "/",
   },
-
   devtool: "source-map",
 
   watchOptions: {
@@ -31,6 +30,36 @@ module.exports = {
     compress: true,
     liveReload: true,
     port: 3000,
+    historyApiFallback: true,
+
+    proxy: {
+      "/ipa/session/json": {
+        target: "https://svcgv.freeipa.cn",
+        secure: false,
+        changeOrigin: true,
+        onProxyReq: (proxyReq) => {
+          const host  = "example.server.ipa.demo"
+          proxyReq.setHeader("origin", `https://${host}/ipa/ui/`);
+          proxyReq.setHeader("referer", `https://${host}/ipa/ui/`);
+          proxyReq.setHeader("host", host);
+          proxyReq.setHeader(
+            "cookie",
+            "ipa_session=MagBearerToken=SaLCj1ZS5ehNgdCxOM7PP8Og%2f9NkZkiCF0YkbhpVxUyM1J0hkVoLPURpHVKZ%2b%2fa77weGp5%2frLml6NcrJj8yUYI2MVp7zClE20vFqO%2fFsOS9pUNxoyvpubnjyrSlmHb9dHQojhk0WOKXsPNCn0Ju0PytGo%2fbKRhMyPHvaaLNDSMRDf%2fHcgAbnshhRPAUgCFgB"
+          );
+          proxyReq.setHeader("Cache-Control", "no-cache");
+          proxyReq.setHeader("content-type", "application/json");
+          proxyReq.setHeader("connection", "keep-alive");
+          proxyReq.setHeader("accept", "*/*");
+          proxyReq.setHeader("accept-encoding", "gzip, deflate, br");
+
+          proxyReq.removeHeader("accept-language");
+          proxyReq.removeHeader("sec-fetch-dest");
+          proxyReq.removeHeader("sec-ch-ua-mobile");
+          proxyReq.removeHeader("sec-ch-ua-platform");
+          proxyReq.removeHeader("sec-ch-ua");
+        },
+      },
+    },
   },
 
   resolve: {
