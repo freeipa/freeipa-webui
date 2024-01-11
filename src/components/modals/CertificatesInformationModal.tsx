d15db9a090dc7d796f62896efb6c970d57e535e8
Carla Martinez <carlmart@redhat.com>
2024-01-11 13:59:01 +0100

    Fix on delete certificates
    There is an error when a Certificate is
    deleted from the 'Settings' page > certificate
    kebab options that leads to a white screen.
    
    Fixes: https://github.com/freeipa/freeipa-webui/issues/251
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/components/modals/CertificatesInformationModal.tsx
+++ src/components/modals/CertificatesInformationModal.tsx
@@ -20,6 +20,17 @@ interface PropsToCertificatesInfoModal {
 }
 
 const CertificatesInformationModal = (props: PropsToCertificatesInfoModal) => {
+  const [certName, setCertName] = React.useState<string>("");
+
+  React.useEffect(() => {
+    if (props.certificatesList[props.idxSelected].certInfo !== undefined) {
+      setCertName(
+        parseDn(props.certificatesList[props.idxSelected].certInfo.subject).cn
+      );
+    }
+  }, [props.certificatesList, props.idxSelected]);
+
+  // Actions
   const infoModalActions = [
     <Button key="close" variant="primary" onClick={props.onClose}>
       Close
@@ -41,96 +52,99 @@ const CertificatesInformationModal = (props: PropsToCertificatesInfoModal) => {
 
   const infoModalContent = (
     <>
-      <TitleLayout
-        id={"info-modal-issued-to"}
-        headingLevel="h2"
-        text={"Issued to"}
-      />
-      {parseKeyValue(
-        "Common name",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.subject)
-          .cn || ""
-      )}
-      {parseKeyValue(
-        "Organization",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.subject).o ||
-          ""
-      )}
-      {parseKeyValue(
-        "Organization unit",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.issuer).ou ||
-          ""
-      )}
-      {parseKeyValue(
-        "Serial number",
-        props.certificatesList[props.idxSelected].certInfo.serial_number || ""
-      )}
-      {parseKeyValue(
-        "Serial number (hex)",
-        props.certificatesList[props.idxSelected].certInfo.serial_number_hex ||
-          ""
-      )}
-      <TitleLayout
-        id={"info-modal-issued-by"}
-        headingLevel="h2"
-        text={"Issued by"}
-        className="pf-v5-u-mt-sm"
-      />
-      {parseKeyValue(
-        "Common name",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.issuer).cn ||
-          ""
-      )}
-      {parseKeyValue(
-        "Organization",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.issuer).o ||
-          ""
-      )}
-      {parseKeyValue(
-        "Organization unit",
-        parseDn(props.certificatesList[props.idxSelected].certInfo.issuer).ou ||
-          ""
-      )}
-      <TitleLayout
-        id={"info-modal-validity"}
-        headingLevel="h2"
-        text={"Validity"}
-        className="pf-v5-u-mt-sm"
-      />
-      {parseKeyValue(
-        "Issued on",
-        props.certificatesList[props.idxSelected].certInfo.valid_not_before ||
-          ""
-      )}
-      {parseKeyValue(
-        "Expires on",
-        props.certificatesList[props.idxSelected].certInfo.valid_not_after || ""
-      )}
-      <TitleLayout
-        id={"info-modal-fingerprints"}
-        headingLevel="h2"
-        text={"Fingerprints"}
-        className="pf-v5-u-mt-sm"
-      />
-      {parseKeyValue(
-        "Expires on",
-        props.certificatesList[props.idxSelected].certInfo.sha1_fingerprint ||
-          ""
-      )}
-      {parseKeyValue(
-        "Expires on",
-        props.certificatesList[props.idxSelected].certInfo.sha256_fingerprint ||
-          ""
+      {props.certificatesList[props.idxSelected].certInfo !== undefined && (
+        <>
+          <TitleLayout
+            id={"info-modal-issued-to"}
+            headingLevel="h2"
+            text={"Issued to"}
+          />
+          {parseKeyValue(
+            "Common name",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.subject)
+              .cn || ""
+          )}
+          {parseKeyValue(
+            "Organization",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.subject)
+              .o || ""
+          )}
+          {parseKeyValue(
+            "Organization unit",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.issuer)
+              .ou || ""
+          )}
+          {parseKeyValue(
+            "Serial number",
+            props.certificatesList[props.idxSelected].certInfo.serial_number ||
+              ""
+          )}
+          {parseKeyValue(
+            "Serial number (hex)",
+            props.certificatesList[props.idxSelected].certInfo
+              .serial_number_hex || ""
+          )}
+          <TitleLayout
+            id={"info-modal-issued-by"}
+            headingLevel="h2"
+            text={"Issued by"}
+            className="pf-v5-u-mt-sm"
+          />
+          {parseKeyValue(
+            "Common name",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.issuer)
+              .cn || ""
+          )}
+          {parseKeyValue(
+            "Organization",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.issuer)
+              .o || ""
+          )}
+          {parseKeyValue(
+            "Organization unit",
+            parseDn(props.certificatesList[props.idxSelected].certInfo.issuer)
+              .ou || ""
+          )}
+          <TitleLayout
+            id={"info-modal-validity"}
+            headingLevel="h2"
+            text={"Validity"}
+            className="pf-v5-u-mt-sm"
+          />
+          {parseKeyValue(
+            "Issued on",
+            props.certificatesList[props.idxSelected].certInfo
+              .valid_not_before || ""
+          )}
+          {parseKeyValue(
+            "Expires on",
+            props.certificatesList[props.idxSelected].certInfo
+              .valid_not_after || ""
+          )}
+          <TitleLayout
+            id={"info-modal-fingerprints"}
+            headingLevel="h2"
+            text={"Fingerprints"}
+            className="pf-v5-u-mt-sm"
+          />
+          {parseKeyValue(
+            "Expires on",
+            props.certificatesList[props.idxSelected].certInfo
+              .sha1_fingerprint || ""
+          )}
+          {parseKeyValue(
+            "Expires on",
+            props.certificatesList[props.idxSelected].certInfo
+              .sha256_fingerprint || ""
+          )}
+        </>
       )}
     </>
   );
 
   return (
     <InformationModalLayout
-      title={
-        "Certificate for " +
-        parseDn(props.certificatesList[props.idxSelected].certInfo.subject).cn
-      }
+      title={"Certificate for " + certName}
       variant="medium"
       actions={infoModalActions}
       isOpen={props.isOpen}
