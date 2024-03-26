99e5158fe83d306af7932e431d047f6f927f8ccb
Mark Reynolds <mreynolds@redhat.com>
2024-03-26 09:38:46 -0400

    Issue 303 - Use tabs for create/retrieve keytab sections
    This PR just covers the "User" sections for Creating/retrieving
    keytab permissions.
    
    I also saw that we had duplicate files, and I was able to condense
    multiple tables/modals into one.
    
    relates: https://github.com/freeipa/freeipa-webui/issues/303
    
    Signed-off-by: Mark Reynolds <mreynolds@redhat.com>

--- src/components/HostsSections/AllowedCreateKeytab.tsx
+++ src/components/HostsSections/AllowedCreateKeytab.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 // PatternFly
-import { Flex, FlexItem } from "@patternfly/react-core";
+import { Label, Tabs, Tab, TabTitleText } from "@patternfly/react-core";
 // Tables
-import CreateKeytabUsersTable from "../tables/HostsSettings/CreateKeytabUsersTable";
+import KeytabUsersTable from "../tables/HostsSettings/KeytabUsersTable";
 import CreateKeytabUserGroupsTable from "../tables/HostsSettings/CreateKeytabUserGroupsTable";
 import CreateKeytabHostsTable from "../tables/HostsSettings/CreateKeytabHostsTable";
 import CreateKeytabHostGroupsTable from "../tables/HostsSettings/CreateKeytabHostGroupsTable";
@@ -15,27 +15,106 @@ interface PropsToAllowCreateKeytab {
 }
 
 const AllowedCreateKeytab = (props: PropsToAllowCreateKeytab) => {
+  const [activeTabKey, setActiveTabKey] = React.useState<string | number>(0);
+
   let fqdn = "";
   if (props.host.fqdn !== undefined) {
     fqdn = props.host.fqdn;
   }
 
+  const handleTabClick = (
+    event: React.MouseEvent | React.KeyboardEvent | MouseEvent,
+    tabIndex: string | number
+  ) => {
+    setActiveTabKey(tabIndex);
+  };
+
+  // Get the item count for the label
+  let attr = "ipaallowedtoperform_write_keys_user";
+  const user_count =
+    props.host[attr] !== undefined ? props.host[attr].length : 0;
+  attr = "ipaallowedtoperform_write_keys_group";
+  const group_count =
+    props.host[attr] !== undefined ? props.host[attr].length : 0;
+  attr = "ipaallowedtoperform_write_keys_host";
+  const host_count =
+    props.host[attr] !== undefined ? props.host[attr].length : 0;
+  attr = "ipaallowedtoperform_write_keys_hostgroup";
+  const hostgroup_count =
+    props.host[attr] !== undefined ? props.host[attr].length : 0;
+
   return (
-    <Flex direction={{ default: "column", lg: "row" }}>
-      <FlexItem flex={{ default: "flex_1" }}>
-        <CreateKeytabUsersTable
+    <Tabs
+      activeKey={activeTabKey}
+      onSelect={handleTabClick}
+      aria-label="Tabs for types of entries that can create keytabs"
+    >
+      <Tab
+        key={0}
+        eventKey={0}
+        title={
+          <TabTitleText>
+            Users <Label isCompact>{user_count}</Label>
+          </TabTitleText>
+        }
+        aria-label="user groups for create keytabs"
+      >
+        <KeytabUsersTable
           from="host"
           id={fqdn}
           entry={props.host}
           onRefresh={props.onRefresh}
+          className="pf-v5-u-ml-md pf-v5-u-mt-sm"
+          opType="create"
+          entryAttr="ipaallowedtoperform_write_keys_user"
         />
-        <CreateKeytabHostsTable host={fqdn} />
-      </FlexItem>
-      <FlexItem flex={{ default: "flex_1" }}>
-        <CreateKeytabUserGroupsTable host={fqdn} />
-        <CreateKeytabHostGroupsTable host={fqdn} />
-      </FlexItem>
-    </Flex>
+      </Tab>
+      <Tab
+        key={1}
+        eventKey={1}
+        title={
+          <TabTitleText>
+            User Groups <Label isCompact>{group_count}</Label>
+          </TabTitleText>
+        }
+        aria-label="user groups for create keytabs"
+      >
+        <CreateKeytabUserGroupsTable
+          host={fqdn}
+          className="pf-v5-u-ml-md pf-v5-u-mt-sm"
+        />
+      </Tab>
+      <Tab
+        key={2}
+        eventKey={2}
+        title={
+          <TabTitleText>
+            Hosts <Label isCompact>{host_count}</Label>
+          </TabTitleText>
+        }
+        aria-label="hosts for create keytabs"
+      >
+        <CreateKeytabHostsTable
+          host={fqdn}
+          className="pf-v5-u-ml-md pf-v5-u-mt-sm"
+        />
+      </Tab>
+      <Tab
+        key={3}
+        eventKey={3}
+        title={
+          <TabTitleText>
+            Host Groups <Label isCompact>{hostgroup_count}</Label>
+          </TabTitleText>
+        }
+        aria-label="hostser groups for create keytabs"
+      >
+        <CreateKeytabHostGroupsTable
+          host={fqdn}
+          className="pf-v5-u-ml-md pf-v5-u-mt-sm"
+        />
+      </Tab>
+    </Tabs>
   );
 };
 
