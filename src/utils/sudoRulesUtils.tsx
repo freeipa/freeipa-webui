c628a1a270c5b26afad5a646fbe83db21eddc877
Carla Martinez <carlmart@redhat.com>
2024-09-17 13:41:04 +0200

    Add missing parameters in 'SudoRule' data type
    The `SudoRule` data type has some missing parameters
    needed to show in the 'Settings' page.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/utils/sudoRulesUtils.tsx
+++ src/utils/sudoRulesUtils.tsx
@@ -38,7 +38,45 @@ export function createEmptySudoRule(): SudoRule {
     dn: "",
     description: "",
     sudoorder: "",
+    externaluser: "",
+    usercategory: "",
+    hostcategory: "",
+    cmdcategory: "",
+    memberuser_user: [],
+    memberhost_host: [],
+    memberhost_hostgroup: [],
+    memberallowcmd_sudocmd: [],
+    memberallowcmd_sudocmdgroup: [],
+    memberdenycmd_sudocmd: [],
+    memberdenycmd_sudocmdgroup: [],
+    ipasudoopt: [],
+    ipasudorunas_user: [],
+    ipasudorunas_group: [],
+    ipasudorunasgroup_group: [],
+    ipasudorunasusercategory: "",
+    ipasudorunasgroupcategory: "",
+    hostmask: "",
+    externalhost: "",
+    ipasudorunasextusergroup: "",
+    ipasudorunasextgroup: "",
+    ipasudorunasextuser: "",
   };
 
   return sudoRule;
 }
+
+// Parse the 'textInputField' data into expected data type
+// - TODO: Adapt it to work with many types of data
+export const asRecord = (
+  element: Partial<SudoRule>,
+  onElementChange: (element: Partial<SudoRule>) => void
+) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ipaObject = element as Record<string, any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function recordOnChange(ipaObject: Record<string, any>) {
+    onElementChange(ipaObject as SudoRule);
+  }
+
+  return { ipaObject, recordOnChange };
+};
