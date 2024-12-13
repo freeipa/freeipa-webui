7937369a25ed4dc8087e07e3d06af5382f0c342e
Carla Martinez <carlmart@redhat.com>
2024-12-13 11:48:08 +0100

    Add timeout in some assertions
    Some assertions might need more time to load
    the data needed from other ones, so a timeout
    must be needed in those cases.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- tests/features/steps/common.ts
+++ tests/features/steps/common.ts
@@ -45,7 +45,8 @@ Given("I am logged in as {string}", (username: string) => {
     }
   });
   cy.get(
-    "div.pf-v5-c-masthead__content button span.pf-v5-c-menu-toggle__text"
+    "div.pf-v5-c-masthead__content button span.pf-v5-c-menu-toggle__text",
+    { timeout: 6000 }
   ).then(($ele) => {
     if ($ele.text() !== username) {
       // @ts-ignore
@@ -314,7 +315,9 @@ Then(
 );
 
 When("I select partial entry {string} in the data table", (name: string) => {
-  cy.get("tr[id^='" + name + "'] input[type=checkbox]").check();
+  cy.get("tr[id^='" + name + "'] input[type=checkbox]", {
+    timeout: 6000,
+  }).check();
 });
 
 // Notifications
