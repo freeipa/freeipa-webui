fa34d9095c4e47cd753184581c96e40b7192a204
Carla Martinez <carlmart@redhat.com>
2025-02-14 16:20:55 +0100

    Change name to general-use components
    As some components related to the automember
    section has multiple reusable components that
    can be used indistictively for `User group rules`
    and `Host group rules`, those can have a more
    generalistic name.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/pages/AutoMemHostRules/AutoMemHostRules.tsx
+++ src/pages/AutoMemHostRules/AutoMemHostRules.tsx
@@ -26,7 +26,7 @@ import ToolbarLayout, {
 } from "src/components/layouts/ToolbarLayout";
 import SearchInputLayout from "src/components/layouts/SearchInputLayout";
 // Tables
-import MainTable from "../AutoMemUserRules/AutomemUserRulesTable";
+import MainTable from "../AutoMemUserRules/AutomemRulesTable";
 // Components
 import PaginationLayout from "../../components/layouts/PaginationLayout";
 import BulkSelectorPrep from "src/components/BulkSelectorPrep";
