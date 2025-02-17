e34868ac9192ba0842e660727c1936410d49a2e3
Carla Martinez <carlmart@redhat.com>
2025-02-17 15:54:41 +0100

    Create a folder to hold component and test
    In order to have a better organization of the components
    and its tests, those should be under a folder with the
    same name as the component itsef. Also, some affected
    components imports must be corrected to the new path.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/components/Form/PrincipalAliasMultiTextBox.tsx
+++ src/components/Form/PrincipalAliasMultiTextBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // PatternFly
 import { Button } from "@patternfly/react-core";
 // Form
-import IpaTextInputFromList from "./IpaTextInputFromList";
+import IpaTextInputFromList from "./IpaTextInputFromList/IpaTextInputFromList";
 // Modals
 import AddTextInputFromListModal from "../modals/AddTextInputFromListModal";
 import ConfirmationModal from "../modals/ConfirmationModal";
