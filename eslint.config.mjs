e9064554afe3a13581488fdeb185ce180715da31
Carla Martinez <carlmart@redhat.com>
2025-04-04 17:22:49 +0200

    Fix EsLint and add prettier ignore
    The `@types/react-router` dependency is not needed
    anymore and it is making the `npm install` and
    `npm run lint` command to fail abruptly.
    
    This needs to be handled by installing and
    configuring ESLint based on the documentation[1]
    and removing the existing `package-lock.json` file
    and `node_modules` folder to do `npm install`
    again. This way, the errors are now fixed.
    
    Apart from that, the `tests/ipalab/_venv` folder
    needs to be added in the `.prettierignore`.
    
    [1] - https://eslint.org/docs/latest/use/getting-started#quick-start
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,61 +1,20 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import reactHooks from "eslint-plugin-react-hooks";
-import { fixupPluginRules } from "@eslint/compat";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
 export default defineConfig([
-  globalIgnores(["node_modules/*", "dist/*", "**/vite.config.ts"]),
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
-    extends: compat.extends(
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended"
-    ),
-
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-      "react-hooks": fixupPluginRules(reactHooks),
-      "react-hooks": fixupPluginRules(reactHooks),
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: "commonjs",
-
-      parserOptions: {
-        tsx: true,
-        jsx: true,
-        js: true,
-        useJSXTextNode: true,
-        project: ["./tsconfig.json", "./cypress/tsconfig.json"],
-        tsconfigRootDir: ".",
-      },
-    },
-
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
