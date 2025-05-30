e65c35c8e007d5ee400999b12f2ebbf5443d5659
Carla Martinez <carlmart@redhat.com>
2025-05-30 16:21:23 +0200

    Certificate id. mapping rule > settings test
    Test for the Certificate identity mapping rules >
    Settings page.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/pages/CertificateMapping/CertificateMappingSettings.tsx
+++ src/pages/CertificateMapping/CertificateMappingSettings.tsx
@@ -323,6 +323,7 @@ const CertificateMappingSettings = (props: CertificateMappingSettingsProps) => {
                     }
                   >
                     <IpaNumberInput
+                      id={"ipacertmappriority"}
                       name={"ipacertmappriority"}
                       aria-label={"Priority number"}
                       ipaObject={ipaObject}
