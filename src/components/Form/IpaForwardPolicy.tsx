e181241619d46e6871502363166c7f4e8aaa598e
Carla Martinez <carlmart@redhat.com>
2025-07-03 09:34:07 +0200

    Define the 'IpaForwardPolicy' component
    The `IpaForwardPolicy` is a special field made by three
    radio buttons that reflects which policy type the current
    DNS zone is using. This field must be adapted to be used
    with the IPA API and get data from metadata.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

---
+++ src/components/Form/IpaForwardPolicy.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+// PatternFly
+import { Flex, FlexItem, Radio } from "@patternfly/react-core";
+// Utils
+import {
+  getParamProperties,
+  IPAParamDefinition,
+  updateIpaObject,
+} from "src/utils/ipaObjectUtils";
+
+const IpaForwardPolicy = (props: IPAParamDefinition) => {
+  const { readOnly, value } = getParamProperties(props);
+
+  // States
+  const [option, setOption] = React.useState<string>(value as string);
+
+  // Handle radio button changes
+  const handleRadioChange = (newValue: string) => {
+    setOption(newValue);
+    if (props.ipaObject && props.onChange) {
+      updateIpaObject(props.ipaObject, props.onChange, newValue, props.name);
+    }
+  };
+
+  React.useEffect(() => {
+    handleRadioChange(value as string);
+  }, [value]);
+
+  return (
+    <Flex>
+      <FlexItem>
+        <Radio
+          id={"forward-first"}
+          key={"forward-first"}
+          name={props.name}
+          label={"Forward first"}
+          value={"first"}
+          readOnly={readOnly}
+          onChange={() => handleRadioChange("first")}
+          isChecked={option === "first"}
+        />
+      </FlexItem>
+      <FlexItem>
+        <Radio
+          id={"forward-only"}
+          key={"forward-only"}
+          name={props.name}
+          label={"Forward only"}
+          value={"only"}
+          readOnly={readOnly}
+          onChange={() => handleRadioChange("only")}
+          isChecked={option === "only"}
+        />
+      </FlexItem>
+      <FlexItem>
+        <Radio
+          id={"forwarding-disabled"}
+          key={"forwarding-disabled"}
+          name={props.name}
+          label={"Forwarding disabled"}
+          value={"none"}
+          readOnly={readOnly}
+          onChange={() => handleRadioChange("none")}
+          isChecked={option === "none"}
+        />
+      </FlexItem>
+    </Flex>
+  );
+};
+
+export default IpaForwardPolicy;
