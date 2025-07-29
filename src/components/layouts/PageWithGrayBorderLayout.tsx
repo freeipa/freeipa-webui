a366371ec0b23e7043f898f2b204d8e01700490c
Carla Martinez <carlmart@redhat.com>
2025-07-29 11:30:51 +0200

    Prep: Allow custom breadcrumb and height
    Make the component more customizable by
    allowing to pass custom breadcrumb and
    height directly to it.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/components/layouts/PageWithGrayBorderLayout.tsx
+++ src/components/layouts/PageWithGrayBorderLayout.tsx
@@ -4,28 +4,37 @@ import { PageSection, PageSectionVariants } from "@patternfly/react-core";
 // Components
 import TitleLayout from "src/components/layouts/TitleLayout";
 import ToolbarLayout, { ToolbarItem } from "./ToolbarLayout";
+import BreadCrumb, { BreadCrumbItem } from "./BreadCrumb";
 
 interface PropsToPageWithGrayBorderLayout {
   pageTitle: string;
   id: string;
   toolbarItems?: ToolbarItem[];
+  breadcrumbItems?: BreadCrumbItem[];
+  height?: string;
   children: React.ReactNode;
 }
 
 const PageWithGrayBorderLayout = (props: PropsToPageWithGrayBorderLayout) => {
   const style: React.CSSProperties = {
     overflowY: "auto",
-    height: `calc(100vh - 278.2px)`,
+    height: props.height || `calc(100vh - 278.2px)`,
   };
 
   return (
     <>
       <PageSection variant={PageSectionVariants.light} className="pf-v5-u-pr-0">
+        {props.breadcrumbItems && (
+          <BreadCrumb
+            className="pf-v5-u-mb-md"
+            breadcrumbItems={props.breadcrumbItems}
+          />
+        )}
         <TitleLayout
           id={props.id}
           text={props.pageTitle}
           headingLevel="h1"
-          className="pf-v5-u-mt-md pf-v5-u-ml-md"
+          className="pf-v5-u-mt-md pf-v5-u-ml-md pf-v5-u-mb-md"
         />
       </PageSection>
       <PageSection
