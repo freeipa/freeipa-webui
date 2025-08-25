4700695c751c134d33bc0e26460fbc2da06fba85
Carla Martinez <carlmart@redhat.com>
2025-08-25 09:47:24 +0200

    Add DNS global configuration page
    The DNS global configuration page allows
    to manage some parameters related to global
    configuration.
    
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/navigation/AppRoutes.tsx
+++ src/navigation/AppRoutes.tsx
@@ -66,6 +66,7 @@ import DnsZonesTabs from "src/pages/DNSZones/DnsZonesTabs";
 import DnsForwardZones from "src/pages/DNSZones/DnsForwardZones";
 import DnsResourceRecordsPreSettings from "src/pages/DNSZones/DnsResourceRecordsPreSettings";
 import DnsServers from "src/pages/DNSZones/DnsServers";
+import DnsGlobalConfig from "src/pages/DNSZones/DnsGlobalConfig";
 
 // Renders routes (React)
 export const AppRoutes = ({ isInitialDataLoaded }): React.ReactElement => {
@@ -487,6 +488,9 @@ export const AppRoutes = ({ isInitialDataLoaded }): React.ReactElement => {
               </Route>
               <Route path="dns-servers">
                 <Route path="" element={<DnsServers />} />
+              </Route>
+              <Route path="dns-global-config">
+                <Route path="" element={<DnsGlobalConfig />} />
               </Route>
               <Route path="configuration" element={<Configuration />} />
               {/* Redirect to Active users page if user is logged in and navigates to the root page */}
