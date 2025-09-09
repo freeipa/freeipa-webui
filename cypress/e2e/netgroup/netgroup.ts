d165a998889bacbce278cf851ddd56f3eb8643de
Veronika Miticka <vmiticka@redhat.com>
2025-09-09 15:49:16 +0200

    Add integration tests for netgroup
    Signed-off-by: Veronika Miticka <vmiticka@redhat.com>

---
+++ cypress/e2e/netgroup/netgroup.ts
@@ -0,0 +1,183 @@
+import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import { loginAsAdmin, logout } from "../common/authentication";
+import { navigateTo } from "../common/navigation";
+import {
+  selectEntry,
+  searchForEntry,
+  entryDoesNotExist,
+  entryExists,
+  checkEntry,
+} from "../common/data_tables";
+import { typeInTextbox } from "../common/ui/textbox";
+import { findEntryInTable } from "../common/settings_table";
+import { addItemToRightList } from "../common/ui/dual_list";
+
+Given("netgroup {string} exists", (groupName: string) => {
+  loginAsAdmin();
+  navigateTo("netgroups");
+
+  cy.dataCy("netgroups-button-add").click();
+  cy.dataCy("add-netgroup-modal").should("exist");
+
+  typeInTextbox("modal-textbox-netgroup-name", groupName);
+  cy.dataCy("modal-textbox-netgroup-name").should("have.value", groupName);
+
+  cy.dataCy("modal-button-add").click();
+  cy.dataCy("add-netgroup-modal").should("not.exist");
+  cy.dataCy("add-netgroup-success").should("exist");
+
+  searchForEntry(groupName);
+  entryExists(groupName);
+  logout();
+});
+
+Given("I delete netgroup {string}", (groupName: string) => {
+  loginAsAdmin();
+  navigateTo("netgroups");
+  selectEntry(groupName);
+
+  cy.dataCy("netgroups-button-delete").click();
+  cy.dataCy("delete-netgroups-modal").should("exist");
+
+  cy.dataCy("modal-button-delete").click();
+  cy.dataCy("delete-netgroups-modal").should("not.exist");
+  cy.dataCy("remove-netgroups-success").should("exist");
+
+  searchForEntry(groupName);
+  entryDoesNotExist(groupName);
+  logout();
+});
+
+type ElementType = "user" | "group" | "host" | "hostgroup" | "externalhost";
+
+const assertElementType = (elementType: string) => {
+  const validElementTypes: ElementType[] = [
+    "user",
+    "group",
+    "host",
+    "hostgroup",
+    "externalhost",
+  ];
+  if (!validElementTypes.includes(elementType as ElementType)) {
+    throw new Error(`Invalid element type: ${elementType}`);
+  }
+};
+
+const ensureTabVisibleAndOpen = (elementType: string) => {
+  const tabDataCy = `netgroups-tab-settings-tab-${elementType}s`;
+  cy.dataCy(tabDataCy).click();
+};
+
+Given(
+  "I have element {string} named {string} in netgroup {string}",
+  (elementType: string, element: string, groupName: string) => {
+    loginAsAdmin();
+    navigateTo(`netgroups/${groupName}`);
+
+    assertElementType(elementType);
+    ensureTabVisibleAndOpen(elementType);
+
+    cy.dataCy(`settings-button-add-${elementType}`).click();
+    cy.dataCy("dual-list-modal").should("exist");
+
+    const itemToAdd =
+      elementType === "host"
+        ? `${element}.${Cypress.env("HOSTNAME")}`
+        : element;
+
+    cy.dataCy("dual-list-search-link").click();
+    addItemToRightList(itemToAdd);
+
+    cy.dataCy("modal-button-add").click();
+    cy.dataCy("dual-list-modal").should("not.exist");
+    cy.dataCy("add-member-success").should("exist");
+
+    findEntryInTable(itemToAdd, elementType);
+    entryExists(itemToAdd);
+
+    logout();
+  }
+);
+
+Given(
+  "I delete element {string} named {string} from netgroup {string}",
+  (elementType: string, element: string, groupName: string) => {
+    loginAsAdmin();
+    navigateTo(`netgroups/${groupName}`);
+
+    assertElementType(elementType);
+    ensureTabVisibleAndOpen(elementType);
+
+    const itemToDelete =
+      elementType === "host"
+        ? `${element}.${Cypress.env("HOSTNAME")}`
+        : element;
+
+    findEntryInTable(itemToDelete, elementType);
+    entryExists(itemToDelete);
+    checkEntry(itemToDelete);
+
+    cy.dataCy(`settings-button-delete-${elementType}`).click();
+    cy.dataCy("remove-netgroup-members-modal").should("exist");
+    entryExists(itemToDelete);
+
+    cy.dataCy("modal-button-delete").click();
+    cy.dataCy("remove-netgroup-members-modal").should("not.exist");
+    cy.dataCy("remove-netgroups-success").should("exist");
+
+    entryDoesNotExist(itemToDelete);
+
+    logout();
+  }
+);
+
+Given(
+  "I have external host {string} in netgroup {string}",
+  (externalHost: string, groupName: string) => {
+    loginAsAdmin();
+    navigateTo(`netgroups/${groupName}`);
+
+    assertElementType("externalhost");
+    ensureTabVisibleAndOpen("externalhost");
+
+    cy.dataCy("settings-button-add-externalHost").click();
+    cy.dataCy("add-external-host-modal").should("exist");
+
+    typeInTextbox("modal-textbox-external-host-name", externalHost);
+
+    cy.dataCy("modal-button-add").click();
+    cy.dataCy("add-external-host-modal").should("not.exist");
+    cy.dataCy("add-member-success").should("exist");
+
+    findEntryInTable(externalHost, "externalHost");
+    entryExists(externalHost);
+
+    logout();
+  }
+);
+
+Given(
+  "I delete external host {string} from netgroup {string}",
+  (externalHost: string, groupName: string) => {
+    loginAsAdmin();
+    navigateTo(`netgroups/${groupName}`);
+
+    assertElementType("externalhost");
+    ensureTabVisibleAndOpen("externalhost");
+
+    findEntryInTable(externalHost, "externalHost");
+    entryExists(externalHost);
+    checkEntry(externalHost);
+
+    cy.dataCy("settings-button-delete-externalHost").click();
+    cy.dataCy("remove-netgroup-members-modal").should("exist");
+
+    cy.dataCy("modal-button-delete").click();
+    cy.dataCy("remove-netgroup-members-modal").should("not.exist");
+    cy.dataCy("remove-netgroups-success").should("exist");
+
+    entryDoesNotExist(externalHost);
+
+    logout();
+  }
+);
---
+++ cypress/e2e/netgroup/netgroup_member_of.feature
@@ -0,0 +1,70 @@
+Feature: Netgroup Member Of manipulation
+    Manage netgroup membership in the "Member of" section
+
+    @seed
+    Scenario: Create netgroups
+        Given netgroup "my_netgroup" exists
+        And netgroup "my_netgroup_2" exists
+
+    @test
+    Scenario: Add a netgroup member into the new netgroup
+        Given I am logged in as admin
+        And I am on "netgroups/my_netgroup" page
+
+        When I click on the "netgroups-tab-memberof" tab
+        Then I should see "netgroups-tab-memberof" tab
+
+        When I click on the "member-of-button-add" button
+        Then I should see "member-of-add-modal" modal
+        And I should see "item-my_netgroup_2" dual list item on the left
+
+        When I click on "item-my_netgroup_2" dual list item
+        Then I should see "item-my_netgroup_2" dual list item selected
+
+        When I click on the "dual-list-add-selected" button
+        Then I should see "item-my_netgroup_2" dual list item on the right
+
+        When I click on the "modal-button-add" button
+        Then I should not see "member-of-add-modal" modal
+        And I should see "add-member-success" alert
+        And I should see "my_netgroup_2" entry in the data table
+
+    @test
+    Scenario: Search for a netgroup member
+        Given I am logged in as admin
+        And I am on "netgroups/my_netgroup" page
+
+        When I click on the "netgroups-tab-memberof" tab
+        Then I should see "netgroups-tab-memberof" tab
+
+        When I search for "my_netgroup_2" in the data table
+        Then I should see "my_netgroup_2" entry in the data table
+        And I should not see "my_netgroup" entry in the data table
+
+        When I search for "notthere" in the data table
+        Then I should not see "notthere" entry in the data table
+        And I should not see "my_netgroup_2" entry in the data table
+
+    @test
+    Scenario: Delete a netgroup member from the netgroup
+        Given I am logged in as admin
+        And I am on "netgroups/my_netgroup" page
+
+        When I click on the "netgroups-tab-memberof" tab
+        Then I should see "netgroups-tab-memberof" tab
+
+        When I select entry "my_netgroup_2" in the data table
+        Then I should see "my_netgroup_2" entry selected in the data table
+        When I click on the "member-of-button-delete" button
+        Then I should see "member-of-delete-modal" modal
+        And I should see "my_netgroup_2" entry in the data table
+
+        When I click on the "modal-button-delete" button
+        Then I should not see "member-of-delete-modal" modal
+        And I should see "remove-netgroups-success" alert
+        And I should not see "my_netgroup_2" entry in the data table
+
+    @cleanup
+    Scenario: Delete netgroups
+        Given I delete netgroup "my_netgroup"
+        And I delete netgroup "my_netgroup_2"
