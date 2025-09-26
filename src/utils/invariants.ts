f2632b3f790536f0528e64b3827749942cc78582
David Hanina <dhanina@redhat.com>
2025-09-26 15:57:12 +0200

    Add safely typed useParams
    Replaces useParams, which may include string | undefined with an
    invariant, that ensures the params must be set.
    I might have gone a little bit overboard with the TypeScript magic.
    
    Signed-off-by: David Hanina <dhanina@redhat.com>

---
+++ src/utils/invariants.ts
@@ -0,0 +1,48 @@
+import { Params } from "react-router-dom";
+import invariant from "tiny-invariant";
+
+type Must<T> = {
+  [P in keyof T]-?: NonNullable<T[P]>;
+};
+
+type Key = string | number | symbol;
+
+export type TupleUnion<U extends Key> = {
+  [S in U]: Exclude<U, S> extends never // for each variant in the union, remove it and..
+    ? [S] // ..stop recursion if it was the last variant
+    : [...TupleUnion<Exclude<U, S>>, S]; // ..recur if not
+}[U]; // extract all values from the object
+
+// https://catchts.com/union-array
+export function invariantNonNullable<T>(
+  value: Readonly<Partial<T> | Params<string>>,
+  properties: TupleUnion<keyof T>
+): asserts value is Must<T> {
+  invariant(
+    value !== undefined && value !== null,
+    "Value is null or undefined"
+  );
+
+  const objectKeys = Object.keys(value);
+
+  invariant(
+    objectKeys.length === properties.length,
+    "Value does not have all properties"
+  );
+
+  invariant(
+    // Keys of objects are always strings, the object is basically Object.keys()
+    objectKeys.every((k) => (properties as TupleUnion<string>).includes(k)),
+    "Value does not have all properties"
+  );
+  if (typeof value === "object") {
+    Object.entries(value).forEach(([k, v]) => {
+      invariant(
+        v !== undefined && v !== null,
+        `Value ${k} is null or undefined`
+      );
+    });
+  }
+}
+
+export { invariant };
