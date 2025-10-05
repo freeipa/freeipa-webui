88a12e7dd65056937647383ba64484f1db2efe47
David Hanina <dhanina@redhat.com>
2025-10-05 09:54:35 +0200

    Babel for production
    Replaces SWC for Babel in production build as Babel is architecture
    agnostic, at the cost of slightly longer build times.
    We keep SWC for development, due to being blazing fast.
    
    Signed-off-by: David Hanina <dhanina@redhat.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,53 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
 import license from "rollup-plugin-license";
 import path from "path";
 
+const getReactPlugin = async (isDev: boolean) => {
+  if (isDev) {
+    return (await import("@vitejs/plugin-react-swc")).default();
+  }
+  return (await import("@vitejs/plugin-react")).default();
+};
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/ipa/modern-ui",
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    react(),
-    license({
-      thirdParty: {
-        output: path.join(__dirname, "dist", "COPYING"),
-        includePrivate: true, // Default is false.
+export default defineConfig(async ({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    base: "/ipa/modern-ui",
+    build: {
+      sourcemap: true,
+    },
+    plugins: [
+      // Use SWC for development, Babel for production (due to s390x and ppc64)
+      await getReactPlugin(isDev),
+      license({
+        thirdParty: {
+          output: path.join(__dirname, "dist", "COPYING"),
+          includePrivate: true, // Default is false.
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        src: "/src",
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      src: "/src",
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    origin: "http://localhost:5173",
-    cors: {
-      origin: "https://webui.ipa.test",
-    },
-  },
-  test: {
-    environment: "jsdom",
     server: {
-      deps: {
-        inline: [/@patternfly\/.*/],
+      host: "0.0.0.0",
+      origin: "http://localhost:5173",
+      cors: {
+        origin: "https://webui.ipa.test",
       },
     },
-    setupFiles: ["./src/setupTests.ts"],
-  },
+    test: {
+      environment: "jsdom",
+      server: {
+        deps: {
+          inline: [/@patternfly\/.*/],
+        },
+      },
+      setupFiles: ["./src/setupTests.ts"],
+    },
+  };
 });
