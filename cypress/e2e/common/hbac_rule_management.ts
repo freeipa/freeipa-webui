db3f3fa80094f270b38d42807159911491d64400
Veronika Miticka <vmiticka@redhat.com>
2025-11-19 11:01:20 +0100

    Replace given seeds with direct ipa calls

--- cypress/e2e/common/hbac_rule_management.ts
+++ cypress/e2e/common/hbac_rule_management.ts
@@ -4,27 +4,11 @@ import {
   selectEntry,
   searchForEntry,
   entryDoesNotExist,
-  entryExists,
 } from "../common/data_tables";
 import { navigateTo } from "../common/navigation";
-import { typeInTextbox } from "../common/ui/textbox";
 
 Given("hbac rule {string} exists", (ruleName: string) => {
-  loginAsAdmin();
-  navigateTo("hbac-rules");
-
-  cy.dataCy("hbac-rules-button-add").click();
-  cy.dataCy("add-hbac-rule-modal").should("exist");
-
-  typeInTextbox("modal-textbox-rule-name", ruleName);
-  cy.dataCy("modal-textbox-rule-name").should("have.value", ruleName);
-
-  cy.dataCy("modal-button-add").click();
-  cy.dataCy("add-hbac-rule-modal").should("not.exist");
-
-  searchForEntry(ruleName);
-  entryExists(ruleName);
-  logout();
+  cy.ipa("hbacrule-add", ruleName, { failOnNonZeroExit: false });
 });
 
 Given("I delete hbac rule {string}", (ruleName: string) => {
