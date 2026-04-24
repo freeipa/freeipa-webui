3f3ca5d49802ac08fc399d6e9781a4cab8cb6803
David Hanina <dhanina@redhat.com>
2026-04-24 15:47:36 +0200

    Script to flatten i18n keys
    The keys from the `i18n_messages` endpoint
    are returned as object types, and replacing
    the i18n keys using the right path will require
    knowing which specific option and types we
    are dealing with. The script aims to generate
    a file (default: `i18n.generated.ts`) to track
    all the flatten routes and have them available
    for replacing.
    
    Co-authored-by: David Hanina <dhanina@redhat.com>
    Signed-off-by: Carla Martinez <carlmart@redhat.com>

--- src/utils/datatypes/Internalization.ts
+++ src/utils/datatypes/Internalization.ts
@@ -1,4 +1,5 @@
 import { ErrorRPCResponse, ValidResponse } from "src/services/rpc";
+import { TRANSLATIONS } from "./InternationalizationKeys";
 
 export type InternalizationResponse =
   | ErrorRPCResponse
@@ -16,20 +17,3 @@ export type TranslationReplaces<K extends TranslationKey> =
     : {
         [P in (typeof TRANSLATIONS)[K][number]]: string;
       };
-
-export const TRANSLATIONS = {
-  "texts.ajax.401.message": [],
-  "texts.actions.apply": [],
-  "texts.actions.automember_rebuild": [],
-  "texts.actions.automember_rebuild_confirm": [],
-  "texts.actions.automember_rebuild_success": [],
-  "texts.actions.confirm": [],
-  "texts.actions.delete_confirm": ["object"],
-  "texts.actions.disable_confirm": ["object"],
-  "texts.actions.enable_confirm": ["object"],
-  "texts.actions.title": [],
-  "texts.association.paging": ["start", "end", "total"],
-  // Example errors:
-  "texts.association.removed": ["error"],
-  "texts.association.error-key": ["start", "end", "total"],
-} as const;
